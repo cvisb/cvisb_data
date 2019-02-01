e7930f7777111b2fb44cd491be45c74b72743ac4
Laura Hughes <flaneuse@users.noreply.github.com>
2019-02-01 14:19:40 -0800

    reverting

--- sample-viewer/src/app/app.module.ts
+++ sample-viewer/src/app/app.module.ts
@@ -9,7 +9,7 @@ import { MaterialModule } from './material.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms'
 import { NgSelectModule } from '@ng-select/ng-select';
 import { HttpModule } from '@angular/http'; // Though outdated, required as per https://github.com/angular/angular/issues/20101 to remove "StaticInjector" error
-import { HttpClient } from "@angular/common/http";
+import { HttpClientModule } from "@angular/common/http";
 import { AdminModule, PipesModule, HlaModule } from '.';
 
 // Services
@@ -126,7 +126,7 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
     BrowserModule.withServerTransition({ appId: 'sample-viewer' }),
     TransferHttpCacheModule,
     HttpModule,
-    HttpClient,
+    HttpClientModule,
     FormsModule,
     ReactiveFormsModule,
     NgSelectModule,
