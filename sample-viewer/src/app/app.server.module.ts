06f04475b22a4a991fe3bfa29ae887beefd439c3
Laura Hughes <flaneuse@users.noreply.github.com>
2019-02-01 13:27:22 -0800

    Add MyHttpClient to app.server

--- sample-viewer/src/app/app.server.module.ts
+++ sample-viewer/src/app/app.server.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { ServerModule, ServerTransferStateModule } from '@angular/platform-server';
 import { ModuleMapLoaderModule } from '@nguniversal/module-map-ngfactory-loader';
+import { MyHttpClient } from './_services/auth-helper.service';
 
 import { AppModule } from './app.module';
 import { AppComponent } from './app.component';
@@ -12,6 +13,7 @@ import { AppComponent } from './app.component';
     ModuleMapLoaderModule,
     ServerTransferStateModule
   ],
+  providers: [MyHttpClient],
   bootstrap: [ AppComponent ],
 })
 export class AppServerModule {}
