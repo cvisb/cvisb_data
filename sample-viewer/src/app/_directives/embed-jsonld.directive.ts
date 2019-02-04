63b4b28b6742305b12d5d81f1b4f16af80260967
Laura Hughes <flaneuse@users.noreply.github.com>
2019-02-04 13:21:57 -0800

    adding in client-side addition of md

--- sample-viewer/src/app/_directives/embed-jsonld.directive.ts
+++ sample-viewer/src/app/_directives/embed-jsonld.directive.ts
@@ -23,12 +23,21 @@ export class EmbedJsonldDirective implements OnDestroy {
       // NOTE: since insertion has to happen server-side to get picked up by Google Dataset Search, it'll be hidden on local client-side rendering (for testing).
       // Server-side appending will occur only on initial page load
       // ... which means if the /dataset page (or anything else) is route-blocked by the guard initially, it WON'T be on the page
+      console.log('server-side')
 
       this.script_element.type = `application/ld+json`;
       this.script_element.text = JSON.stringify(jsonObj);
       this.script_element.title = `schemaorg ${jsonType}`;
       this.script_element.id = `${jsonType}-${jsonObj['name']}`
       this.renderer.appendChild(this._document.head, this.script_element);
+    } else {
+      console.log('client-side')
+
+            this.script_element.type = `application/ld+json`;
+            this.script_element.text = JSON.stringify(jsonObj);
+            this.script_element.title = `schemaorg ${jsonType}`;
+            this.script_element.id = `${jsonType}-${jsonObj['name']}`
+            this.renderer.appendChild(this._document.head, this.script_element);
     }
   }
 
