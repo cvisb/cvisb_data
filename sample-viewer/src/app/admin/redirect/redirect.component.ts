426329518c6fc2160cebdc5c027fb246317e3b86
Laura Hughes <flaneuse@users.noreply.github.com>
2019-02-04 14:22:39 -0800

    move embed outside ngIf

--- sample-viewer/src/app/admin/redirect/redirect.component.ts
+++ sample-viewer/src/app/admin/redirect/redirect.component.ts
@@ -19,11 +19,7 @@ export class RedirectComponent implements OnInit {
     private authSvc: AuthService
   ) {
 
-    console.log("REDIRECT PAGE CONSTRUCTED")
-
     route.queryParams.subscribe(params => {
-      console.log(params);
-      console.log("redirect sees next page = " + params['next']);
       this.page_url = params['next']
     })
 
@@ -31,9 +27,6 @@ export class RedirectComponent implements OnInit {
 
   ngOnInit() {
     this.authSvc.checkLogin().then(result => {
-      console.log('Auth service has completed. Then is returning a status of: ' + result)
-
-      console.log("Redirect is trying to redirect to " + this.page_url)
       if (this.page_url) {
         this.router.navigate([this.page_url])
       }
