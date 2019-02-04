9896596fe162e672be613a750c761ffd5965a5c6
Laura Hughes <flaneuse@users.noreply.github.com>
2019-02-04 12:56:22 -0800

    make /dataset public

--- sample-viewer/src/app/_directives/embed-jsonld.directive.ts
+++ sample-viewer/src/app/_directives/embed-jsonld.directive.ts
@@ -20,24 +20,14 @@ export class EmbedJsonldDirective implements OnDestroy {
       // NOTE: insertion needs to occur on server-side ONLY-- or else you get two copies.
       // NOTE: since insertion has to happen server-side to get picked up by Google Dataset Search, it'll be hidden on local client-side rendering (for testing).
       // Server-side appending will occur only on initial page load
-      // console.log('appending ' + jsonType + " server-side")
+      // ... which means if the /dataset page (or anything else) is route-blocked by the guard initially, it WON'T be on the page
 
       this.script_element.type = `application/ld+json`;
       this.script_element.text = JSON.stringify(jsonObj);
       this.script_element.title = `schemaorg ${jsonType}`;
       this.script_element.id = `${jsonType}-${jsonObj['name']}`
-      // this.script_element.id = `schemaorg-client`;
-      // console.log('appending ' + jsonType + " client-side")
       this.renderer.appendChild(this._document.head, this.script_element);
     }
-    // else {
-    //   // console.log('appending ' + jsonType + " client-side")
-    //   this.script_element.type = `application/ld+json`;
-    //   this.script_element.text = JSON.stringify(jsonObj);
-    //   this.script_element.title = `schemaorg ${jsonType}`;
-    //   this.script_element.id = `${jsonType}-${jsonObj['name']}`
-    //   this.renderer.appendChild(this._document.head, this.script_element);
-    // }
   }
 
   constructor(
@@ -47,9 +37,8 @@ export class EmbedJsonldDirective implements OnDestroy {
   ) {
   }
 
-  // Clean up past additions to the
+  // Clean up past additions to the page
   ngOnDestroy(): void {
-    // console.log('on destroy')
     // NOTE: should also happen server-side
     if (this.script_element) {
       this.renderer.removeChild(this._document.head, this.script_element);
