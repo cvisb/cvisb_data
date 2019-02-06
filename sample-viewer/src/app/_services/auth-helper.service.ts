ff2ce95b494b9b9beb8afe99bf0bc00e6398750e
Laura Hughes <flaneuse@users.noreply.github.com>
2019-02-06 15:08:51 -0800

    Update auth-helper.service.ts

--- sample-viewer/src/app/_services/auth-helper.service.ts
+++ sample-viewer/src/app/_services/auth-helper.service.ts
@@ -52,25 +52,25 @@ export class MyHttpClient extends HttpClient {
     options.withCredentials = true;
 
     // if we are server side, then import cookie header from express
-    // if (isPlatformServer(this.platformId)) {
+    if (isPlatformServer(this.platformId)) {
     // console.log(this.injector)
     // const req: any = this.injector.get('REQUEST');
-    // const rawCookies = !!req.headers['cookie'] ? req.headers['cookie'] : '';
+    const rawCookies = !!this.req.headers['cookie'] ? this.req.headers['cookie'] : '';
 
-    console.log(this.req)
+    // console.log(this.req)
     // console.log(rawCookies)
-    console.log(first)
-    console.log(url)
-    console.log(options)
+    // console.log(first)
+    // console.log(url)
+    // console.log(options)
 
-    //   if (typeof first !== "string")
-    //     first = (first as HttpRequest<any>).clone({ setHeaders: { 'cookie': rawCookies } });
-    //   options.headers = (options.headers as HttpHeaders).set('cookie', rawCookies);
-    // }
+      if (typeof first !== "string")
+        first = (first as HttpRequest<any>).clone({ setHeaders: { 'cookie': rawCookies } });
+      options.headers = (options.headers as HttpHeaders).set('cookie', rawCookies);
+    }
 
-    return null
+    // return null
 
-    // return super.request(first as (any), url, options);
+    return super.request(first as (any), url, options);
   }
 
 }
