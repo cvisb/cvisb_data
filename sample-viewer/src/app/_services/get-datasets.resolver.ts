76cb7c8b63eeab0ab939298b68706244772bfe2e
Laura Hughes <flaneuse@users.noreply.github.com>
2019-02-07 15:34:07 -0800

    clean up comments

--- sample-viewer/src/app/_services/get-datasets.resolver.ts
+++ sample-viewer/src/app/_services/get-datasets.resolver.ts
@@ -1,3 +1,6 @@
+// Resolver is used to make sure the call to getDataset completes *before* Angular Universal finishes rendering the Dataset
+// Necessary so Google Dataset search sees the dataset metadata being embedded in the page source as it crawls.
+
 import { Injectable } from '@angular/core';
 
 import { Resolve, ActivatedRouteSnapshot } from '@angular/router';
@@ -11,7 +14,6 @@ export class DatasetResolver implements Resolve<any> {
   constructor(private datasetSvc: getDatasetsService) {}
 
   resolve(route: ActivatedRouteSnapshot) {
-    console.log(route)
     return this.datasetSvc.getDataset(route.paramMap.get('dsid'));
   }
 }
