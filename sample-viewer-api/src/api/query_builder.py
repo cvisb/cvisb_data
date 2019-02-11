c4d3ef71efd730ad4cc135f8c9ee7a617456cd56
Ubuntu <cyrus.afrasiabi@gmail.com>
2019-02-11 17:25:59 +0000

    Making document version return in /query endpoints

--- sample-viewer-api/src/api/query_builder.py
+++ sample-viewer-api/src/api/query_builder.py
@@ -2,4 +2,7 @@ from biothings.web.api.es.query_builder import ESQueryBuilder
 
 class ESQueryBuilder(ESQueryBuilder):
     # Implement app specific queries here
-    pass
+    def _return_query_kwargs(self, query_kwargs):
+        _kwargs = {"index": self.index, "doc_type": self.doc_type, "version": True}
+        _kwargs.update(query_kwargs)
+        return _kwargs
