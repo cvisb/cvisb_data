1e8dad99789012e9c15b7d89f0af4da3178c60fd
Laura Hughes <flaneuse@users.noreply.github.com>
2019-02-12 13:57:15 -0800

    activate requestparams, PUT test data

--- sample-viewer/src/app/_models/request-param.ts
+++ sample-viewer/src/app/_models/request-param.ts
@@ -8,15 +8,43 @@ export class RequestParam {
     this.value = value;
   }
 
-  // collapse to ES syntax
-  // https://www.elastic.co/guide/en/elasticsearch/reference/current/query-dsl-query-string-query.html
-  // Example compound query:http://18.235.94.96/api/sample/query?q=patient_id:("S024") location:KGH
-  collapse() {
-    if (Array.isArray(this.value)) {
-      return (`${this.field}:\(\"${this.value.join('" "')}\"\)`)
-    } else {
-      return (`${this.field}:\"${this.value}\"`)
-    }
 
-  }
 }
+
+export class RequestParamArray extends Array<RequestParam> {
+  constructor(items) {
+    super();
+    this.push(...items)
+  }
+  // collapse(): any {
+  //   console.log("collapsing")
+  //   return('string')
+  // }
+
+}
+
+// export class RequestParamArray extends Array<RequestParam> {
+//   private constructor(items?: Array<RequestParam>) {
+//     super(...items)
+//     Object.setPrototypeOf(this, Object.create(RequestParamArray.prototype));
+//   }
+//
+//   //   create(): RequestParamArray {
+//   //     return Object.create(RequestParamArray.prototype);
+//   // }
+//
+//
+//   // collapse Object to ES syntax string
+//   // https://www.elastic.co/guide/en/elasticsearch/reference/current/query-dsl-query-string-query.html
+//   // Example compound query:http://18.235.94.96/api/sample/query?q=patient_id:("S024") location:KGH
+//   collapse(): any {
+//     console.log('collapsing')
+//     return (this);
+//     // if (Array.isArray(this.value)) {
+//     //   return (`${this.field}:\(\"${this.value.join('" "')}\"\)`)
+//     // } else {
+//     //   return (`${this.field}:\"${this.value}\"`)
+//     // }
+//
+//   }
+// }
