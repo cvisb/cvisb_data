7f5d7d746dbeec813366d860d3d350858ed20935
Laura Hughes <flaneuse@users.noreply.github.com>
2019-02-13 15:57:47 -0800

    adding in outcome filtering

--- sample-viewer/src/app/filters/mini-barplot/mini-barplot.component.ts
+++ sample-viewer/src/app/filters/mini-barplot/mini-barplot.component.ts
@@ -5,6 +5,9 @@ import * as d3 from 'd3';
 
 import { StripWhitespacePipe } from '../../_pipes';
 
+// Event listeners to update the search query
+import { GetPatientsService } from '../../_services';
+
 @Component({
   selector: 'app-mini-barplot',
   templateUrl: './mini-barplot.component.html',
@@ -15,6 +18,7 @@ import { StripWhitespacePipe } from '../../_pipes';
 export class MiniBarplotComponent implements OnInit {
   @ViewChild('barplot') private chartContainer: ElementRef;
   @Input() private data: any;
+  @Input() private endpoint: any;
   @Input() private height: number;
   @Input() private spacing: number;
   @Input() private name_var: string;
@@ -51,6 +55,7 @@ export class MiniBarplotComponent implements OnInit {
 
   // constructor() { }
   constructor(private strip: StripWhitespacePipe,
+    private patientSvc: GetPatientsService,
     // Whether to be rendered server-side or client-side
     @Inject(PLATFORM_ID) private platformId: Object
   ) { }
@@ -97,11 +102,11 @@ export class MiniBarplotComponent implements OnInit {
     this.bars_annotations = this.chart.append("g")
       .attr("class", 'bar--annot');
 
-      this.ylabels = this.chart
-        .append("g")
-        .attr("class", 'y-label');
+    this.ylabels = this.chart
+      .append("g")
+      .attr("class", 'y-label');
 
-// --- call function to add data, with transition time = 0 ---
+    // --- call function to add data, with transition time = 0 ---
     this.updatePlot(0);
   }
 
@@ -110,6 +115,31 @@ export class MiniBarplotComponent implements OnInit {
       // transition
       var t = d3.transition()
         .duration(tDuration);
+
+      // Handle into filtering by virus type
+      let filterOutcome = function(endpoint: string, patientSvc: any) {
+        return function(d) {
+          console.log(d)
+          switch (endpoint) {
+            case 'patient': {
+              let params = [{ field: 'outcome', value: d.key }];
+
+              patientSvc.patientParamsSubject.next(params);
+              break;
+            }
+            case 'dataset': {
+              console.log('sending dataset endpoint cohort ' + d.key);
+              let params = [{ field: 'outcome', value: d.key }];
+              console.log(params)
+              break;
+            }
+            default: {
+              console.log("ERROR! Unknown endpoint to be filtered.")
+              break;
+            }
+          }
+        }
+      }
 
       // --- Update domains ---
       this.x.domain([0, <any>d3.max(this.data, (d: any) => d.value)]);
@@ -147,18 +177,21 @@ export class MiniBarplotComponent implements OnInit {
         .attr("x", (d: any) => this.x(d.value))
         .attr("width", (d: any) => this.x(0) - this.x(d.value));
 
+      this.chart.selectAll("rect")
+        .on("click", filterOutcome(this.endpoint, this.patientSvc));
+
 
       // --- Annotate bars ---
       let bars_labels = this.bars_annotations.selectAll("text")
         .data(this.data);
 
-        bars_labels.exit()
-          .transition(t)
-          .style("fill-opacity", 1e-6)
-          .remove();
+      bars_labels.exit()
+        .transition(t)
+        .style("fill-opacity", 1e-6)
+        .remove();
 
 
-        bars_labels.enter().append("text")
+      bars_labels.enter().append("text")
         .attr("class", "annotation")
         .attr("id", (d: any) => this.strip.transform(d[this.name_var]))
         .merge(bars_labels)
@@ -193,41 +226,41 @@ export class MiniBarplotComponent implements OnInit {
         .text((d: any) => (d[this.name_var]));
 
 
-        // Container for the annotation
-        // this.ylabels
-        //   .selectAll(".y-label")
-        //   .data(this.data).enter().append("g")
-        //   .attr("id", (d: any) => this.strip.transform(d[this.name_var]));
-        //
-        // // Dummy rectangle, if need to turn into a button
-        // ylabels.append("rect");
-        //
-        // // Actual label for the annotation
-        // ylabels.append("text")
-        //   .attr("class", "annotation")
-        //   .attr("x", (d: any) => this.x(0))
-        //   .attr("y", (d: any) => this.y(d[this.name_var]) + this.y.bandwidth() / 2)
-        //   .attr("dx", 6)
-        //   .style("font-size", Math.min(this.y.bandwidth(), 14))
-        //   .text((d: any) => (d[this.name_var]));
-        //
-        // let getTextBox = function(selection) {
-        //   selection.each(function(d) { d.bbox = this.getBBox(); })
-        // };
-        //
-        // if (this.name_var === 'type') {
-        //   ylabels.selectAll('text')
-        //     .attr("dx", 10)
-        //     .style("font-size", Math.min(this.y.bandwidth(), 14) * 0.8)
-        //     .call(getTextBox);
-        //
-        //   ylabels.insert("rect", "text")
-        //     .attr("class", "rect faux-button")
-        //     .attr("x", function(d) { return d.bbox.x - 4 })
-        //     .attr("y", function(d) { return d.bbox.y - 2 })
-        //     .attr("width", function(d) { return d.bbox.width + 8 })
-        //     .attr("height", function(d) { return d.bbox.height + 4 })
-        // }
+      // Container for the annotation
+      // this.ylabels
+      //   .selectAll(".y-label")
+      //   .data(this.data).enter().append("g")
+      //   .attr("id", (d: any) => this.strip.transform(d[this.name_var]));
+      //
+      // // Dummy rectangle, if need to turn into a button
+      // ylabels.append("rect");
+      //
+      // // Actual label for the annotation
+      // ylabels.append("text")
+      //   .attr("class", "annotation")
+      //   .attr("x", (d: any) => this.x(0))
+      //   .attr("y", (d: any) => this.y(d[this.name_var]) + this.y.bandwidth() / 2)
+      //   .attr("dx", 6)
+      //   .style("font-size", Math.min(this.y.bandwidth(), 14))
+      //   .text((d: any) => (d[this.name_var]));
+      //
+      // let getTextBox = function(selection) {
+      //   selection.each(function(d) { d.bbox = this.getBBox(); })
+      // };
+      //
+      // if (this.name_var === 'type') {
+      //   ylabels.selectAll('text')
+      //     .attr("dx", 10)
+      //     .style("font-size", Math.min(this.y.bandwidth(), 14) * 0.8)
+      //     .call(getTextBox);
+      //
+      //   ylabels.insert("rect", "text")
+      //     .attr("class", "rect faux-button")
+      //     .attr("x", function(d) { return d.bbox.x - 4 })
+      //     .attr("y", function(d) { return d.bbox.y - 2 })
+      //     .attr("width", function(d) { return d.bbox.width + 8 })
+      //     .attr("height", function(d) { return d.bbox.height + 4 })
+      // }
     }
   }
 
