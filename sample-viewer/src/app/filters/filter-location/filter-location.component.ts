7caa12e5168e961739eb26e1bddcc69c6b02226d
Laura Hughes <flaneuse@users.noreply.github.com>
2019-02-14 13:44:02 -0800

    clean up code

--- sample-viewer/src/app/filters/filter-location/filter-location.component.ts
+++ sample-viewer/src/app/filters/filter-location/filter-location.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 
 import { RequestParametersService } from '../../_services';
-import { RequestParam, RequestParamArray } from '../../_models';
+import { RequestParam } from '../../_models';
 
 @Component({
   selector: 'app-filter-location',
@@ -12,12 +12,8 @@ import { RequestParam, RequestParamArray } from '../../_models';
 export class FilterLocationComponent implements OnInit {
   @Input() countries;
   @Input() endpoint: string;
-  params: RequestParamArray;
 
   constructor(private requestSvc: RequestParametersService) {
-    requestSvc.patientParamsState$.subscribe((params: RequestParamArray) => {
-      this.params = params;
-    })
    }
 
   ngOnInit() {
@@ -25,23 +21,6 @@ export class FilterLocationComponent implements OnInit {
 
   selectCountry(ctry: string) {
     this.requestSvc.updateParams(this.endpoint, { field: 'country.identifier', value: ctry })
-    // switch (this.endpoint) {
-    //   case 'patient': {
-    //     this.params.push({field: 'country.identifier', value: ctry});
-    //     this.params = [{field: 'country.identifier', value: ctry}];
-    //
-    //     this.requestSvc.patientParamsSubject.next(this.params);
-    //     break;
-    //   }
-    //   case 'dataset': {
-    //     console.log('sending dataset endpoint country ' + ctry);
-    //     break;
-    //   }
-    //   default: {
-    //     console.log("ERROR! Unknown endpoint to be filtered.")
-    //     break;
-    //   }
-    // }
   }
 
 }
