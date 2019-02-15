221218e3e9c622b80eeb59908087fbca364e7449
Laura Hughes <flaneuse@users.noreply.github.com>
2019-02-14 16:25:56 -0800

    + filter patient IDs

--- sample-viewer/src/app/filters/filter-patient-id/filter-patient-id.component.ts
+++ sample-viewer/src/app/filters/filter-patient-id/filter-patient-id.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 
+import { RequestParametersService } from '../../_services';
 
 @Component({
   selector: 'app-filter-patient-id',
@@ -7,13 +8,19 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./filter-patient-id.component.scss']
 })
 export class FilterPatientIdComponent implements OnInit {
+  @Input() endpoint: string;
   @Input() patients: string[];
 
   selectedPatients: string[];
 
-  constructor() { }
+  constructor(private requestSvc: RequestParametersService) { }
 
   ngOnInit() {
+  }
+
+  filterPatientIDs(ids) {
+    console.log(ids);
+    this.requestSvc.updateParams(endpoint, { field: 'patientID', value: ids })
   }
 
 }
