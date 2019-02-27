5d1beb68b0186d0e649d57ed73d761f72ac029a0
Laura Hughes <flaneuse@users.noreply.github.com>
2019-02-27 10:43:13 -0800

    add file upload / PUT

---
+++ sample-viewer/src/app/add-samples/sample-upload/sample-upload.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit, OnChanges } from '@angular/core';
+
+import { ApiService } from '../../_services/api.service';
+
+@Component({
+  selector: 'app-sample-upload',
+  templateUrl: './sample-upload.component.html',
+  styleUrls: ['./sample-upload.component.scss']
+})
+
+// https://blog.teamtreehouse.com/reading-files-using-the-html5-filereader-api
+// https://developer.mozilla.org/en-US/docs/Web/API/FileReader
+export class SampleUploadComponent implements OnInit {
+
+  constructor(private apiSvc: ApiService) { }
+
+  ngOnInit() {
+  }
+
+  ngOnChanges() {
+  }
+
+  fileChange(event) {
+    console.log(event)
+
+    let fileList: FileList = event.target.files;
+
+    if (fileList.length > 0) {
+      let file: File = fileList[0];
+
+      var reader = new FileReader();
+
+      // Read in the file as a text object.
+      reader.readAsText(file, 'application/json');
+
+      // listen for the file to be loaded; then save the result.
+      reader.onload = (e) => {
+        let data = JSON.parse(reader.result);
+        console.log(data)
+        this.apiSvc.put("sample", data)
+      }
+    }
+
+  }
+}
