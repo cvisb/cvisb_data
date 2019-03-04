f3da5becc06e98dee911f4dacc657d9733d3cdf8
Laura Hughes <flaneuse@users.noreply.github.com>
2019-03-04 13:56:24 -0800

    hard code/ sync variable names

--- sample-viewer/src/app/_dialogs/sample-metadata/sample-metadata.component.ts
+++ sample-viewer/src/app/_dialogs/sample-metadata/sample-metadata.component.ts
@@ -11,14 +11,13 @@ import SAMPLE_SCHEMA from '../../../assets/schema/sample.json';
 })
 export class SampleMetadataComponent {
 
-  sample_vars: string[];
+  sample_vars: string[] = ["alternateIdentifier", "patientID", "privatePatientID", "visitCode", "isolationDate", "sampleType", "derivedIndex", "species", "protocolVersion", "protocolURL", "freezingBuffer", "dilutionFactor", "AVLinactivated", "dateModified", "_version"];
   vars2ignore: string[] = ['location', 'num_aliquots', 'updated', 'updated_by'];
 
   constructor(public dialogRef: MatDialogRef<SampleMetadataComponent>,
     @Inject(MAT_DIALOG_DATA) public data: Object) {
-    this.sample_vars = Object.keys(SAMPLE_SCHEMA.properties);
+    // this.sample_vars = Object.keys(SAMPLE_SCHEMA.properties);
 
-    let iterable = [10, 20, 30];
 
     for (let value of this.vars2ignore) {
       this.sample_vars.splice(this.sample_vars.findIndex(d => d === value), 1);
