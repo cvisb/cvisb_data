d429bf675be2ac2e99571916d0eca0096aac209e
Laura Hughes <flaneuse@users.noreply.github.com>
2019-03-12 09:43:12 -0700

    flatten location, temp remove cohort, etc.

--- sample-viewer/src/app/_services/lab-locations.service.ts
+++ sample-viewer/src/app/_services/lab-locations.service.ts
@@ -12,8 +12,8 @@ export class LabLocationsService {
     { 'id': 'TSRI-Andersen', 'short_label': 'TSRI: Andersen', 'label': 'TSRI: KGA', 'category': 'TSRI' },
     { 'id': 'TSRI-Sullivan', 'label': 'TSRI: Sullivan', 'short_label': 'TSRI: BS', 'category': 'TSRI' },
     { 'id': 'TSRI-Briney', 'label': 'TSRI: Briney', 'short_label': 'TSRI: BB', 'category': 'TSRI' },
-    { 'id': 'Tulane-Garry', 'label': 'Tulane-Garry', 'short_label': 'Tulane: RG', 'category': 'Tulane' },
-    { 'id': 'Tulane-Schieffelin', 'label': 'Tulane-Schieffelin', 'short_label': 'Tulane: JS', 'category': 'Tulane' },
+    { 'id': 'Tulane-Garry', 'label': 'Tulane: Garry', 'short_label': 'Tulane: RG', 'category': 'Tulane' },
+    { 'id': 'Tulane-Schieffelin', 'label': 'Tulane: Schieffelin', 'short_label': 'Tulane: JS', 'category': 'Tulane' },
     { 'id': 'MGH-Alter', 'label': 'MGH: Alter', 'short_label': 'MGH: GA', 'category': 'MGH' }
   ];
 
