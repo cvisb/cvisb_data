7b7721abf726eff6bb5ebc7db270da1d5930ed62
Laura Hughes <flaneuse@users.noreply.github.com>
2019-04-04 15:26:18 -0700

    update home content

--- sample-viewer/src/app/home/home.component.ts
+++ sample-viewer/src/app/home/home.component.ts
@@ -21,11 +21,12 @@ import DOWNLOADS from '../../assets/data/test_datadownloads.json';
 export class HomeComponent implements OnInit {
 
   cards: Object[] = [
-    { label: "Explore CViSB data", path: "dataset", description: "We will follow a strict release of open access data within two weeks of data generation. Please check back here for more updates as soon as the data starts coming off the machines." },
-    { label: "View CViSB schema", path: "schema", description: "We know it's one thing to share data-- and another to make it FAIR. To improve the *findability* and *interoperability* of our data, we've created schemas for our dataset, patient, and experiment metadata built off of [schema.org](https://schema.org/)'s model.", tbd: false },
-    { label: "View CViSB patients", path: "patient", description: "View and search metadata for all current patients", warning: "CViSB-members only", tbd: true },
+    { label: "Explore CViSB data", path: "dataset", description: "We will follow a strict release of open access data within two weeks of data generation.", warning: "CViSB-members only" },
+    { label: "View CViSB patients", path: "patient", description: "View and search metadata for all current patients", warning: "CViSB-members only", tbd: false },
     { label: "View CViSB samples", path: "sample", description: "View and search location and metadata for all current samples", warning: "CViSB-members only" },
-    { label: "Add or update CViSB samples", path: "sample/upload", description: "Add or change the metadata or location information for a sample", warning: "CViSB-members only", tbd: true },
+    { label: "About CViSB", path: "about", description: "Learn more about the Center for Viral Systems Biology" },
+    { label: "Understand CViSB data", path: "documentation", description: "We know it's one thing to share data-- and another to make it FAIR. To improve the *findability* and *interoperability* of our data, we've created schemas for our dataset, patient, and experiment metadata built off of [schema.org](https://schema.org/)'s model.", tbd: false },
+    { label: "Add or update CViSB data", path: "upload", description: "Add or change the metadata for datasets, patients, and samples", warning: "CViSB-members only", tbd: true },
 
   ];
 
