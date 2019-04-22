8bc844758e0a1bb94e06d2f92a7838a721c79b26
Laura Hughes <flaneuse@users.noreply.github.com>
2019-04-22 16:30:38 -0700

    filter data based on sampleID

---
+++ sample-viewer/src/app/_pipes/filter-old-data.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { FilterOldDataPipe } from './filter-old-data.pipe';
+
+describe('FilterOldDataPipe', () => {
+  it('create an instance', () => {
+    const pipe = new FilterOldDataPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
