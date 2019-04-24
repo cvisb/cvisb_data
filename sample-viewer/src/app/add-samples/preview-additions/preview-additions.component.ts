b0b09996f6178c8a268fe2a12da58256ea3e7045
Laura Hughes <flaneuse@users.noreply.github.com>
2019-04-24 14:25:41 -0700

    initial preview additions

---
+++ sample-viewer/src/app/add-samples/preview-additions/preview-additions.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-preview-additions',
+  templateUrl: './preview-additions.component.html',
+  styleUrls: ['./preview-additions.component.scss']
+})
+export class PreviewAdditionsComponent implements OnInit {
+
+  previewData: Object[];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
