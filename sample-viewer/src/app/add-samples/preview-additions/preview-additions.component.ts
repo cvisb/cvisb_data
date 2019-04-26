5c02defcde9bd6b160c590c04ea7d4915e0b9925
Laura Hughes <flaneuse@users.noreply.github.com>
2019-04-26 12:50:05 -0700

    connect previewData to behaviorsubject

--- sample-viewer/src/app/add-samples/preview-additions/preview-additions.component.ts
+++ sample-viewer/src/app/add-samples/preview-additions/preview-additions.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 
+import { SampleUploadService } from '../../_services';
+
 @Component({
   selector: 'app-preview-additions',
   templateUrl: './preview-additions.component.html',
   styleUrls: ['./preview-additions.component.scss']
 })
+
 export class PreviewAdditionsComponent implements OnInit {
 
   previewData: Object[];
 
-  constructor() { }
+  constructor(uploadSvc: SampleUploadService) {
+    uploadSvc.uploadSamplesState$.subscribe(merged => {
+
+    this.previewData = merged.filter((d:any) => d._merge === "right_only");
+    console.log(this.previewData);
+  })
+
+   }
 
   ngOnInit() {
   }
