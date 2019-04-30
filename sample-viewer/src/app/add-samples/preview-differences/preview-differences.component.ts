e4cae7bb9af4cf59f63b7b3d742f2dfcbbf75e72
Laura Hughes <flaneuse@users.noreply.github.com>
2019-04-30 10:48:27 -0700

    add sorting of cols, fix ngIf

--- sample-viewer/src/app/add-samples/preview-differences/preview-differences.component.ts
+++ sample-viewer/src/app/add-samples/preview-differences/preview-differences.component.ts
@@ -15,6 +15,8 @@ export class PreviewDifferencesComponent implements OnInit {
   locationColumns: string[];
   errorMsg: string;
 
+  columnOrder = ["sampleID", "sampleLabel", "privatePatientID", "visitCode", "location", "sampleType", "isolationDate", "_id"];
+
   @ViewChild(MatPaginator) paginator: MatPaginator;
   @ViewChild(MatSort) sort: MatSort;
 
@@ -23,18 +25,31 @@ export class PreviewDifferencesComponent implements OnInit {
 
       let merged = mergedObj.merged;
       this.displayedColumns = mergedObj.displayedColumns;
+      this.displayedColumns.sort((a, b) => this.sortingFunc(a) - this.sortingFunc(b));
+
       this.locationColumns = mergedObj.locationColumns;
 
-      this.dataSource = new MatTableDataSource(merged.filter(d => d._merge === "both"));
-      this.dataSource.paginator = this.paginator;
-      this.dataSource.sort = this.sort;
+      if (merged && merged.length > 0) {
+        this.dataSource = new MatTableDataSource(merged.filter(d => d._merge === "both"));
+        this.dataSource.paginator = this.paginator;
+        this.dataSource.sort = this.sort;
+      } else {
+        this.dataSource = new MatTableDataSource();
+      }
     })
 
   }
 
   ngOnInit() {
+  }
 
-
+  sortingFunc(a) {
+    let idx = this.columnOrder.indexOf(a);
+    // if not found, return dummy so sorts at the end
+    if (idx < 0) {
+      return (1000);
+    }
+    return (idx);
   }
 
 }
