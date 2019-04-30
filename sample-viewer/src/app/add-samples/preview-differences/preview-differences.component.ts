8033817def45133ec152ca16e7d536a9c15e154e
Laura Hughes <flaneuse@users.noreply.github.com>
2019-04-30 10:41:12 -0700

    reset dataSource to nothing if no data

--- sample-viewer/src/app/add-samples/preview-differences/preview-differences.component.ts
+++ sample-viewer/src/app/add-samples/preview-differences/preview-differences.component.ts
@@ -25,11 +25,9 @@ export class PreviewDifferencesComponent implements OnInit {
       this.displayedColumns = mergedObj.displayedColumns;
       this.locationColumns = mergedObj.locationColumns;
 
-      if (merged && merged.length > 0) {
-        this.dataSource = new MatTableDataSource(merged.filter(d => d._merge === "both"));
-        this.dataSource.paginator = this.paginator;
-        this.dataSource.sort = this.sort;
-      }
+      this.dataSource = new MatTableDataSource(merged.filter(d => d._merge === "both"));
+      this.dataSource.paginator = this.paginator;
+      this.dataSource.sort = this.sort;
     })
 
   }
