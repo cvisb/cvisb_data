f716b1cb4d2051cba283adc3bf3a0d262506ee23
Laura Hughes <flaneuse@users.noreply.github.com>
2019-04-29 17:02:10 -0700

    remove filterOldPipe

--- sample-viewer/src/app/_pipes/filter-old-data.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { FilterOldDataPipe } from './filter-old-data.pipe';
-
-describe('FilterOldDataPipe', () => {
-  it('create an instance', () => {
-    const pipe = new FilterOldDataPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
