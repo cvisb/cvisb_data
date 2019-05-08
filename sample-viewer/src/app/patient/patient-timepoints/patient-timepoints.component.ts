a2b9df9cf95afa67dd4e070ae9a42d90050dd7b1
Laura Hughes <flaneuse@users.noreply.github.com>
2019-05-08 10:40:50 -0700

    test API call to get samples for a patient

--- sample-viewer/src/app/patient/patient-timepoints/patient-timepoints.component.ts
+++ sample-viewer/src/app/patient/patient-timepoints/patient-timepoints.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, Input } from '@angular/core';
 
 import { Sample } from '../../_models';
 
+import { ApiService } from '../../_services';
+
 @Component({
   selector: 'app-patient-timepoints',
   templateUrl: './patient-timepoints.component.html',
@@ -9,11 +11,24 @@ import { Sample } from '../../_models';
 })
 
 export class PatientTimepointsComponent implements OnInit {
-  @Input() samples: Sample[];
-  constructor() { }
+  @Input() alternateIDs: string[];
+  private samples: Object[] = [];
+
+  constructor(private apiSvc: ApiService) { }
 
   ngOnInit() {
+    console.log(this.alternateIDs);
+
+    this.alternateIDs.forEach(id => {
+      this.apiSvc.getOne('sample', id, 'privatePatientID').subscribe(res => {
+        console.log(res);
+        this.samples = this.samples.concat(res);
+      })
+    })
     console.log(this.samples)
+
+    this.samples = [{"visitCode": "1", "sampleType": "viralRNA"}, {"visitCode": "1", "sampleType": "PBMC"},{"visitCode": "2", "sampleType": "viralRNA"},{"visitCode": "1", "sampleType": "DNA"}];
+
   }
 
 }
