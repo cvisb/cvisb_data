73090f09723aa7c76fb936125da5a186272daed6
Laura Hughes <flaneuse@users.noreply.github.com>
2019-05-15 14:40:57 -0700

    add dateRange pipe

--- sample-viewer/src/app/pipes/pipes.module.ts
+++ sample-viewer/src/app/pipes/pipes.module.ts
@@ -4,14 +4,15 @@ import { CommonModule } from '@angular/common';
 import { StripWhitespacePipe } from '../_pipes/strip-whitespace.pipe';
 import { FilterLocusPipe } from '../_pipes/filter-locus.pipe';
 import { GetKeysPipe } from '../_pipes/get-keys.pipe';
+import { DateRangePipe } from '../_pipes/date-range.pipe';
 // import { StripWhitespacePipe, SloppyMarkdownPipe } from '../_pipes';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [ StripWhitespacePipe, FilterLocusPipe, GetKeysPipe ],
-  providers: [ StripWhitespacePipe, FilterLocusPipe, GetKeysPipe ],
-  exports: [ StripWhitespacePipe, FilterLocusPipe, GetKeysPipe ]
+  declarations: [ StripWhitespacePipe, FilterLocusPipe, GetKeysPipe, DateRangePipe ],
+  providers: [ StripWhitespacePipe, FilterLocusPipe, GetKeysPipe, DateRangePipe ],
+  exports: [ StripWhitespacePipe, FilterLocusPipe, GetKeysPipe, DateRangePipe ]
 })
 export class PipesModule { }
