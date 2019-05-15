6b7c9a30fa4d84440f64e8ac4fb08e8af887fc30
Laura Hughes <flaneuse@users.noreply.github.com>
2019-05-15 15:25:29 -0700

    remove spec.ts

--- sample-viewer/src/app/_pipes/date-range.pipe.ts
+++ sample-viewer/src/app/_pipes/date-range.pipe.ts
@@ -7,21 +7,21 @@ import { DatePipe } from '@angular/common';
 })
 
 export class DateRangePipe extends DatePipe implements PipeTransform {
-  // transform(value: any, dateFormat: string = "d MMMM y"): any {
-  //   let dateString: string;
-  //
-  //   switch (typeof (value)) {
-  //     case "string":
-  //       dateString = super.transform(value, dateFormat);
-  //       break;
-  //     case "object":
-  //       let lower = super.transform(value.gte, dateFormat);
-  //       let upper = super.transform(value.lte, dateFormat);
-  //
-  //       dateString = lower === upper ? lower : `${lower}-${upper}`;
-  //       break;
-  //   }
-  //
-  //   return dateString;
-  // }
+  transform(value: any, dateFormat: string = "d MMMM y"): any {
+    let dateString: string;
+
+    switch (typeof (value)) {
+      case "string":
+        dateString = super.transform(value, dateFormat);
+        break;
+      case "object":
+        let lower = super.transform(value.gte, dateFormat);
+        let upper = super.transform(value.lte, dateFormat);
+
+        dateString = lower === upper ? lower : `${lower}-${upper}`;
+        break;
+    }
+
+    return dateString;
+  }
 }
