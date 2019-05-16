fd3289ec47e3dd6b12a3e27bf3b29aa9c0660e61
Laura Hughes <flaneuse@users.noreply.github.com>
2019-05-15 18:22:26 -0700

    temp remove infectionYear (dateRange issue)

--- sample-viewer/src/app/patient/filter-patients/filter-patients.component.ts
+++ sample-viewer/src/app/patient/filter-patients/filter-patients.component.ts
@@ -98,8 +98,8 @@ export class FilterPatientsComponent implements OnInit {
       this.all_cohorts = pList.patientTypes.map(d => d.term);
       this.all_outcomes = pList.patientOutcomes.map(d => d.term);
 
-      this.all_years = pList.patientYears.filter((d: any) => Number.isInteger(d.term)).map((d: any) => d.term);
-      this.all_years.sort();
+      // this.all_years = pList.patientYears.filter((d: any) => Number.isInteger(d.term)).map((d: any) => d.term);
+      // this.all_years.sort();
       if (this.all_years.length === 0) {
         this.all_years = [2013, 2014, 2015, 2016, 2017, 2018, 2019];
       }
