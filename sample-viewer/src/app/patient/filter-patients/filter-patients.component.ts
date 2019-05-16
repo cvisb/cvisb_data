8ff46651294e4136850b2a2d44630fa829528f5b
Laura Hughes <flaneuse@users.noreply.github.com>
2019-05-16 15:54:04 -0700

    remove infectionYear ref (temp)

--- sample-viewer/src/app/patient/filter-patients/filter-patients.component.ts
+++ sample-viewer/src/app/patient/filter-patients/filter-patients.component.ts
@@ -100,9 +100,9 @@ export class FilterPatientsComponent implements OnInit {
 
       // this.all_years = pList.patientYears.filter((d: any) => Number.isInteger(d.term)).map((d: any) => d.term);
       // this.all_years.sort();
-      if (this.all_years.length === 0) {
-        this.all_years = [2013, 2014, 2015, 2016, 2017, 2018, 2019];
-      }
+      // if (this.all_years.length === 0) {
+      //   this.all_years = [2013, 2014, 2015, 2016, 2017, 2018, 2019];
+      // }
 
       this.all_countries = pList.patientCountries.map(d => d.term);
 
