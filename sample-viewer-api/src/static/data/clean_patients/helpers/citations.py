9ac5ef0ecedb98efebab4ae07e87997110730bf6
Laura Hughes <flaneuse@users.noreply.github.com>
2019-05-20 12:38:24 -0700

    Update citations.py

--- sample-viewer-api/src/static/data/clean_patients/helpers/citations.py
+++ sample-viewer-api/src/static/data/clean_patients/helpers/citations.py
@@ -1,4 +1,8 @@
 ncbi_stub = 'https://api.ncbi.nlm.nih.gov/lit/ctxp/v1/pubmed/?format=csl&id='
 # example: 'https://api.ncbi.nlm.nih.gov/lit/ctxp/v1/pubmed/?format=csl&id=26276630'
-      .set('id', pmid)
-          .set('format', 'csl')
+import requests
+
+def getCitation(pmid, ncbi_stub = ncbi_stub):
+    res = requests.get(ncbi_stub + str(pmid))
+    if(res.status_code == 200):
+        return(res.json())
