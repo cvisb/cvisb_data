0328af93ca843dd1eb43fc0a4a8add09f2b94747
Laura Hughes <flaneuse@users.noreply.github.com>
2019-05-20 12:38:32 -0700

    Update clean_viral_seq.py

--- sample-viewer-api/src/static/data/clean_viral_seq/clean_viral_seq.py
+++ sample-viewer-api/src/static/data/clean_viral_seq/clean_viral_seq.py
@@ -13,9 +13,9 @@ import helpers
 today = datetime.today().strftime('%Y-%m-%d')
 
 exptCols = ['privatePatientID', 'experimentID', 'GenBank_ID',
-            'measurementTechnique', 'publisher', 'publication', 'data']
+            'measurementTechnique', 'publisher', 'publication', 'data', 'dateModified']
 
-def getPublisher(row, varName="CViSB_data"):
+def getPublisher(row, varName="cvisb_data"):
     # Check binary if CVISB_data
     if(row[varName]):
         return([{
@@ -30,13 +30,14 @@ def getPublisher(row, varName="CViSB_data"):
             }
         }])
 
-def getDNAseq(all_seq, df, df_id):
+def getDNAseq(all_seq, df, df_id, virus):
     for seq in all_seq:
         id = seq.id.split("|")[0]
         if(sum(df[df_id] == id) == 0):
             print(seq.id)
-        df.loc[df[df_id] == id, 'data'] = {"DNAsequence": str(seq.seq)}
-    return(df)
+        seq_obj = {"DNAsequence": str(seq.seq)}
+        df.at[df[df_id] == id, 'data'] = seq_obj
+    return([df])
 
 expt_dir = "/Users/laurahughes/GitHub/cvisb_data/sample-viewer-api/src/static/data/output_data/experiments/"
 
@@ -68,15 +69,16 @@ ebv['experimentID'] = ebv.apply(
 ebv['GenBank_ID'] = ebv.ncbi_accession
 ebv['measurementTechnique'] = "viral sequencing"
 ebv['dateModified'] = today
-ebv['CViSB_data'] = False
+ebv['cvisb_data'] = False
 ebv['publisher'] = ebv.apply(getPublisher, axis=1)
-ebv['publication'] = None
+ebv['publication'] = ebv.publisher.apply(helpers.getCitation)
 
 # Read in Ebola sequence data
 ebv_seq = list(SeqIO.parse(
     "/Users/laurahughes/GitHub/cvisb_data/sample-viewer-api/src/static/data/input_data/expt_summary_data/viral_seq/ebola_wg_ntseq_names_02152019_orfs.fasta", "fasta"))
 ebv['data'] = None
-ebv = getDNAseq(ebv_seq, ebv, 'sample_id')
+ebv['data'] = ebv['data'].astype(object)
+ebv = getDNAseq(ebv_seq, ebv, 'sample_id', "Ebola")
 sum(ebv.data.apply(lambda x: x is None))
 
 ebv[ebv.data.apply(lambda x: x is None)]
@@ -99,21 +101,16 @@ ebv.loc[ebv.mergeIssue == ebv.mergeIssue, ['sample_id', 'ncbi_accession',
 ebv.groupby('KGH_id')._merge.value_counts()
 ebv.columns
 
-ebv.groupby("publication").KGH_id.value_counts()
-
-ebv.iloc[253]
 
 # [Lassa]  ----------------------------------------------------------------------------------------------------
 lsv = pd.read_csv(lsv_file)
 
 
-lsv.columns
-lsv.head()
-
 lsv['privatePatientID'] = lsv.sample_id.apply(helpers.interpretID)
 
 lsv['outcome'] = lsv.outcome.apply(helpers.cleanOutcome)
 lsv['cohort'] = "Lassa"
+lsv['cvisb_data'] = False
 lsv['countryName'] = lsv.country
 
 lsv['KGH_id'] = lsv.sample_id.apply(
@@ -125,14 +122,14 @@ lsv['GenBank_ID'] = lsv.ncbi_accession
 lsv['measurementTechnique'] = "viral sequencing"
 lsv['dateModified'] = today
 lsv['publisher'] = lsv.apply(getPublisher, axis=1)
-lsv['publication'] = lsv.Source_PMID
-
+lsv['publication'] = lsv.Source_PMID.apply(helpers.getCitation)
+lsv.iloc[3]
 # Import DNA sequences
 lsv_seq = list(SeqIO.parse(
     "/Users/laurahughes/GitHub/cvisb_data/sample-viewer-api/src/static/data/input_data/expt_summary_data/viral_seq/Sseg_lasv_mafft_aln_04102019.fasta", "fasta"))
 lsv['data'] = None
-lsv = getDNAseq(lsv_seq, lsv, 'sample_id')
-
+lsv = getDNAseq(lsv_seq, lsv, 'sample_id', "Lassa")
+lsv[exptCols].iloc[9].to_json()
 sum(lsv.data.apply(lambda x: x is None))
 
 lsv[exptCols].sample(4)
@@ -156,4 +153,4 @@ sum((lsv.countryName_x != lsv.countryName_y) &
 
 # [Merge and export: experimental data]  ----------------------------------------------------------------------------------------------------
 expts = pd.concat([ebv[exptCols], lsv[exptCols]])
-expts.to_json(f"{expt_dir}viral_seq_{today}.json", orient="records")
+expts.iloc[500:505].to_json(f"{expt_dir}viral_seq_{today}_1.json", orient="records")
