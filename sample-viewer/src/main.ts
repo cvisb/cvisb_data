3fe9eadef4527df4bb6b61473ac7a6dcebca533e
Laura Hughes <flaneuse@users.noreply.github.com>
2019-05-21 12:07:57 -0700

    cleanup

--- sample-viewer/src/main.ts
+++ sample-viewer/src/main.ts
@@ -4,11 +4,6 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
-// import 'autotrack/lib/plugins/event-tracker';
-// import 'autotrack/lib/plugins/outbound-link-tracker';
-// import 'autotrack/lib/plugins/url-change-tracker';
-// https://github.com/angular/angular-cli/issues/4451
-
 if (environment.production) {
   enableProdMode();
 }
