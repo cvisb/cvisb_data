bc698f14cf036c10c28e827a8947def51744acc8
Ubuntu <cyrus.afrasiabi@gmail.com>
2019-05-30 14:47:59 +0000

    Moving endpoint definitions to its own file, and importing it from config_web

--- sample-viewer-api/src/config_web.py
+++ sample-viewer-api/src/config_web.py
@@ -1,4 +1,5 @@
 from biothings.web.settings.default import *
+from config_cvisb_endpoints import CVISB_ENDPOINTS
 from api.query_builder import ESQueryBuilder
 from api.query import ESQuery
 from api.transform import ESResultTransformer
@@ -13,45 +14,6 @@ SCHEMA_DIR = os.path.join(os.path.split(os.path.abspath(__file__))[0], 'static',
 # *****************************************************************************
 # elasticsearch server transport url
 ES_HOST = 'localhost:9200'
-
-CVISB_ENDPOINTS = {
-    "sample": {
-        "cache_key": "Sample",
-        "index": 'sample_metadata_current',
-        "public_permitted_search_fields": ['AVLinactivated', 'protocolURL', 'derivedIndex', 'freezingBuffer', 'species', 'dateModified', 'dilutionFactor', 'patientID', 'sampleType', 'protocolVersion', 'visitCode', 'sampleGroup'],
-        "public_excluded_return_fields": ['location.*', 'description', 'alternateIdentifier', 'updatedBy', 'privatePatientID', 'sampleLabel', 'isolationDate', 'sampleID', 'name']
-    },
-    "datadownload": {
-        "cache_key": "DataDownload",
-        "index": 'datadownload_metadata_current',
-        "public_permitted_search_fields": ['measurementTechnique', 'publication.*', 'dateCreated', 'sameAs', 'dateModified', 'sourceCode.*', 'contentUrlRepository', 'datePublished', 'includedInDataset', 'keywords', 'contentUrlIdentifier', 'contentUrl', 'name', 'description', 'identifier', '@id', 'encodingFormat', 'experimentIDs', 'author.*', 'additionalType', 'isBasedOn', 'publisher.*'],
-        "public_excluded_return_fields": ['updatedBy']
-    },
-    "dataset": {
-        "cache_key": "Dataset",
-        "index": 'dataset_metadata_current',
-        "public_permitted_search_fields": ['variableMeasured', 'measurementTechnique', 'publication.*', 'sameAs', 'spatialCoverage.*', 'dateModified', 'sourceCode.*', 'license', 'funder.*', 'datePublished', 'url', 'schemaVersion', 'keywords', 'name', 'includedInDataCatalog', 'description', 'citation', 'identifier', '@id', 'author.*', 'dataDownloadIDs', 'temporalCoverage.*', 'publisher.*'],
-        "public_excluded_return_fields": ['updatedBy']
-    },
-    "experiment": {
-        "cache_key": "Experiment",
-        "index": 'experiment_metadata_current',
-        "public_permitted_search_fields": ['experimentID', 'description', 'measurementTechnique', 'publication.*', 'SRA_ID', 'dateModified', 'GenBank_ID', 'analysisCode.*', 'patientID', 'publisher.*', 'name', 'batchID', 'experimentDate', 'data.*'],
-        "public_excluded_return_fields": ['sampleID', 'privatePatientID', 'updatedBy']
-    },
-    "patient": {
-        "cache_key": "Patient",
-        "index": 'patient_metadata_current',
-        "public_permitted_search_fields": ['daysInHospital', 'symptoms.*', 'associatedSamples', 'age', 'relatedTo', 'gender', 'sameAs', 'infectionYear', 'dateModified', 'rtpcr.*', 'availableData.*', 'patientID', 'daysOnset', 'outcome', 'cohort', 'elisa.*', 'country.*', 'rapidDiagostics.*', 'contactGroupIdentifier'],
-        "public_excluded_return_fields": ['updatedBy', 'alternateIdentifier', 'homeLocation.*', 'contactSurvivorRelationship', 'infectionDate.*', 'evalDate', 'admitDate', 'updateNotes.*', 'dischargeDate', 'relatedToPrivate', 'sID', '_source', 'exposureType', 'gID'] 
-    },
-    "datacatalog": {
-        "cache_key": "DataCatalog",
-        "index": 'datacatalog_metadata_current',
-        "public_permitted_search_fields": ['dataset', 'schemaVersion', 'description', 'keywords', 'identifier', '@id', 'alternateName', 'sameAs', 'temporalCoverage.*', 'spatialCoverage.*', 'dateModified', 'funder.*', 'publisher.*', 'name', 'datePublished', 'url'],
-        "public_excluded_return_fields": ['updatedBy']
-    }
-}
 
 # *****************************************************************************
 # App URL Patterns
@@ -88,10 +50,10 @@ QUERY_GET_ESQB_KWARGS['infectionYear'] = {'default': None, 'type': str}
 QUERY_GET_ESQB_KWARGS['measurementTechnique'] = {'default': None, 'type': list}
 QUERY_GET_ESQB_KWARGS['length'] = {'default': None, 'type': str}
 QUERY_GET_ESQB_KWARGS['facet_size']['max'] = 10000
-QUERY_GET_ES_KWARGS['_source']['default'] = {"includes": ["*"], "excludes": []}
-QUERY_POST_ES_KWARGS['_source']['default'] = {"includes": ["*"], "excludes": []}
-ANNOTATION_GET_ES_KWARGS['_source']['default'] = {"includes": ["*"], "excludes": []}
-ANNOTATION_POST_ES_KWARGS['_source']['default'] = {"includes": ["*"], "excludes": []}
+#QUERY_GET_ES_KWARGS['_source']['default'] = {"includes": ["*"], "excludes": []}
+#QUERY_POST_ES_KWARGS['_source']['default'] = {"includes": ["*"], "excludes": []}
+#ANNOTATION_GET_ES_KWARGS['_source']['default'] = {"includes": ["*"], "excludes": []}
+#ANNOTATION_POST_ES_KWARGS['_source']['default'] = {"includes": ["*"], "excludes": []}
 
 ###############################################################################
 #    app-specific query builder, query, and result transformer classes
