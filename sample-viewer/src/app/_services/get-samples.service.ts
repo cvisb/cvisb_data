6d0b38ab6f9660ea354b2b45a4bce414208e1c0c
Laura Hughes <flaneuse@users.noreply.github.com>
2019-05-30 12:43:52 -0700

    add expt, country filter

--- sample-viewer/src/app/_services/get-samples.service.ts
+++ sample-viewer/src/app/_services/get-samples.service.ts
@@ -238,6 +238,7 @@ export class GetSamplesService {
     summary['outcome'] = this.countBy(samples, "outcome", "unknown");
     summary['cohort'] = this.countBy(samples, "cohort", "Unknown");
     summary['years'] = this.countBy(samples, "infectionYear", "Unknown");
+    summary['country'] = this.countBy(samples, "country", "Unknown");
     // Make sure to remove nulls-- ngSelect can't have nulls as options
     summary['patients'] = samples.map(d => d.alternateIdentifier).flat().filter(d => d).sort((a, b) => a < b ? -1 : 1);
 
--- sample-viewer/src/app/filters/filter-experiment/filter-experiment.component.ts
+++ sample-viewer/src/app/filters/filter-experiment/filter-experiment.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
 
-import { D3Nested } from '../../_models';
+// import { D3Nested } from '../../_models';
+//
+import { RequestParametersService } from '../../_services';
 
 @Component({
   selector: 'app-filter-experiment',
@@ -8,12 +10,26 @@ import { D3Nested } from '../../_models';
   styleUrls: ['./filter-experiment.component.scss']
 })
 export class FilterExperimentComponent implements OnInit {
+  @Input() endpoint: string;
+  expts: any[];
 
-  @Input() expts: D3Nested[];
-
-  constructor() { }
+  constructor(private requestSvc: RequestParametersService) { }
 
   ngOnInit() {
+    this.expts = [
+      { key: "hla", name: "HLA sequencing", disabled: true },
+      { key: "viralseq", name: "viral sequencing", disabled: true },
+      { key: "systemsserology", name: "systems serology", disabled: true },
+
+    ];
+  }
+
+  filterExpt(idx) {
+    this.expts[idx].disabled = !this.expts[idx].disabled;
+
+    let exptNames = this.expts.filter(d => !d.disabled).map(d => d.name);
+
+    this.requestSvc.updateParams(this.endpoint, { field: "measurementTechnique", value: exptNames });
   }
 
 }
