8af248ad0eaacdefb0bb4d462ebfe9d3da1001cb
Ubuntu <cyrus.afrasiabi@gmail.com>
2019-06-04 19:26:19 +0000

    Removing some old properties from cvisb_stuff

--- sample-viewer-api/src/config_cvisb_endpoints.py
+++ sample-viewer-api/src/config_cvisb_endpoints.py
@@ -9,11 +9,11 @@ CVISB_DEFAULT_SETTINGS = {
 CVISB_ENDPOINTS = {
     "sample": {
         "cache_key": "Sample",
-        "index": 'sample_metadata_current',
+        #"index": 'sample_metadata_current',
         "private_index": "sample_metadata_current",
         "public_index": "sample_metadata_public_current",
-        "public_permitted_search_fields": ['AVLinactivated', 'protocolURL', 'derivedIndex', 'freezingBuffer', 'species', 'dateModified', 'dilutionFactor', 'patientID', 'sampleType', 'protocolVersion', 'visitCode', 'sampleGroup'],
-        "public_excluded_return_fields": ['location.*', 'description', 'alternateIdentifier', 'updatedBy', 'privatePatientID', 'sampleLabel', 'isolationDate', 'sampleID', 'name'],
+        #"public_permitted_search_fields": ['AVLinactivated', 'protocolURL', 'derivedIndex', 'freezingBuffer', 'species', 'dateModified', 'dilutionFactor', 'patientID', 'sampleType', 'protocolVersion', 'visitCode', 'sampleGroup'],
+        #"public_excluded_return_fields": ['location.*', 'description', 'alternateIdentifier', 'updatedBy', 'privatePatientID', 'sampleLabel', 'isolationDate', 'sampleID', 'name'],
         "private_fields": ['location.*', 'description', 'alternateIdentifier', 'updatedBy', 'privatePatientID', 'sampleLabel', 'isolationDate', 'sampleID', 'name'],
         "elasticsearch_public_index_body": {
             "settings": CVISB_DEFAULT_SETTINGS,
@@ -36,11 +36,11 @@ CVISB_ENDPOINTS = {
     },
     "datadownload": {
         "cache_key": "DataDownload",
-        "index": 'datadownload_metadata_current',
+        #"index": 'datadownload_metadata_current',
         "private_index": "datadownload_metadata_current",
         "public_index": "datadownload_metadata_public_current",
-        "public_permitted_search_fields": ['measurementTechnique', 'publication.*', 'dateCreated', 'sameAs', 'dateModified', 'sourceCode.*', 'contentUrlRepository', 'datePublished', 'includedInDataset', 'keywords', 'contentUrlIdentifier', 'contentUrl', 'name', 'description', 'identifier', '@id', 'encodingFormat', 'experimentIDs', 'author.*', 'additionalType', 'isBasedOn', 'publisher.*'],
-        "public_excluded_return_fields": ['updatedBy'],
+        #"public_permitted_search_fields": ['measurementTechnique', 'publication.*', 'dateCreated', 'sameAs', 'dateModified', 'sourceCode.*', 'contentUrlRepository', 'datePublished', 'includedInDataset', 'keywords', 'contentUrlIdentifier', 'contentUrl', 'name', 'description', 'identifier', '@id', 'encodingFormat', 'experimentIDs', 'author.*', 'additionalType', 'isBasedOn', 'publisher.*'],
+        #"public_excluded_return_fields": ['updatedBy'],
         "private_fields": ['updatedBy'],
         "elasticsearch_public_index_body": {
             "settings": CVISB_DEFAULT_SETTINGS,
@@ -63,11 +63,11 @@ CVISB_ENDPOINTS = {
     },
     "dataset": {
         "cache_key": "Dataset",
-        "index": 'dataset_metadata_current',
+        #"index": 'dataset_metadata_current',
         "private_index": 'dataset_metadata_current',
         "public_index": 'dataset_metadata_public_current',
-        "public_permitted_search_fields": ['variableMeasured', 'measurementTechnique', 'publication.*', 'sameAs', 'spatialCoverage.*', 'dateModified', 'sourceCode.*', 'license', 'funder.*', 'datePublished', 'url', 'schemaVersion', 'keywords', 'name', 'includedInDataCatalog', 'description', 'citation', 'identifier', '@id', 'author.*', 'dataDownloadIDs', 'temporalCoverage.*', 'publisher.*'],
-        "public_excluded_return_fields": ['updatedBy'],
+        #"public_permitted_search_fields": ['variableMeasured', 'measurementTechnique', 'publication.*', 'sameAs', 'spatialCoverage.*', 'dateModified', 'sourceCode.*', 'license', 'funder.*', 'datePublished', 'url', 'schemaVersion', 'keywords', 'name', 'includedInDataCatalog', 'description', 'citation', 'identifier', '@id', 'author.*', 'dataDownloadIDs', 'temporalCoverage.*', 'publisher.*'],
+        #"public_excluded_return_fields": ['updatedBy'],
         "private_fields": ['updatedBy'],
         "elasticsearch_private_index_body": {
             "settings": CVISB_DEFAULT_SETTINGS,
@@ -114,11 +114,11 @@ CVISB_ENDPOINTS = {
     },
     "experiment": {
         "cache_key": "Experiment",
-        "index": 'experiment_metadata_current',
+        #"index": 'experiment_metadata_current',
         "private_index": 'experiment_metadata_current',
         "public_index": 'experiment_metadata_public_current',
-        "public_permitted_search_fields": ['experimentID', 'description', 'measurementTechnique', 'publication.*', 'SRA_ID', 'dateModified', 'GenBank_ID', 'analysisCode.*', 'patientID', 'publisher.*', 'name', 'batchID', 'experimentDate', 'data.*'],
-        "public_excluded_return_fields": ['sampleID', 'privatePatientID', 'updatedBy'],
+        #"public_permitted_search_fields": ['experimentID', 'description', 'measurementTechnique', 'publication.*', 'SRA_ID', 'dateModified', 'GenBank_ID', 'analysisCode.*', 'patientID', 'publisher.*', 'name', 'batchID', 'experimentDate', 'data.*'],
+        #"public_excluded_return_fields": ['sampleID', 'privatePatientID', 'updatedBy'],
         "private_fields": ['sampleID', 'privatePatientID', 'updatedBy'],
         "elasticsearch_private_index_body": {
             "settings": CVISB_DEFAULT_SETTINGS,
@@ -141,11 +141,11 @@ CVISB_ENDPOINTS = {
     },
     "patient": {
         "cache_key": "Patient",
-        "index": 'patient_metadata_current',
+        #"index": 'patient_metadata_current',
         "public_index": 'patient_metadata_public_current',
         "private_index": 'patient_metadata_current',
-        "public_permitted_search_fields": ['daysInHospital', 'symptoms.*', 'associatedSamples', 'age', 'relatedTo', 'gender', 'sameAs', 'infectionYear', 'dateModified', 'rtpcr.*', 'availableData.*', 'patientID', 'daysOnset', 'outcome', 'cohort', 'elisa.*', 'country.*', 'rapidDiagostics.*', 'contactGroupIdentifier'],
-        "public_excluded_return_fields": ['updatedBy', 'alternateIdentifier', 'homeLocation.*', 'contactSurvivorRelationship', 'infectionDate.*', 'evalDate', 'admitDate', 'updateNotes.*', 'dischargeDate', 'relatedToPrivate', 'sID', 'exposureType', 'gID'],
+        #"public_permitted_search_fields": ['daysInHospital', 'symptoms.*', 'associatedSamples', 'age', 'relatedTo', 'gender', 'sameAs', 'infectionYear', 'dateModified', 'rtpcr.*', 'availableData.*', 'patientID', 'daysOnset', 'outcome', 'cohort', 'elisa.*', 'country.*', 'rapidDiagostics.*', 'contactGroupIdentifier'],
+        #"public_excluded_return_fields": ['updatedBy', 'alternateIdentifier', 'homeLocation.*', 'contactSurvivorRelationship', 'infectionDate.*', 'evalDate', 'admitDate', 'updateNotes.*', 'dischargeDate', 'relatedToPrivate', 'sID', 'exposureType', 'gID'],
         "private_fields": ['updatedBy', 'alternateIdentifier', 'homeLocation.*', 'contactSurvivorRelationship', 'infectionDate.*', 'evalDate', 'admitDate', 'updateNotes.*', 'dischargeDate', 'relatedToPrivate', 'sID', 'exposureType', 'gID'],
         "elasticsearch_private_index_body": {
             "settings": CVISB_DEFAULT_SETTINGS,
@@ -230,11 +230,11 @@ CVISB_ENDPOINTS = {
     },
     "datacatalog": {
         "cache_key": "DataCatalog",
-        "index": 'datacatalog_metadata_current',
+        #"index": 'datacatalog_metadata_current',
         "private_index": 'datacatalog_metadata_current',
         "public_index": 'datacatalog_metadata_public_current',
-        "public_permitted_search_fields": ['dataset', 'schemaVersion', 'description', 'keywords', 'identifier', '@id', 'alternateName', 'sameAs', 'temporalCoverage.*', 'spatialCoverage.*', 'dateModified', 'funder.*', 'publisher.*', 'name', 'datePublished', 'url'],
-        "public_excluded_return_fields": ['updatedBy'],
+        #"public_permitted_search_fields": ['dataset', 'schemaVersion', 'description', 'keywords', 'identifier', '@id', 'alternateName', 'sameAs', 'temporalCoverage.*', 'spatialCoverage.*', 'dateModified', 'funder.*', 'publisher.*', 'name', 'datePublished', 'url'],
+        #"public_excluded_return_fields": ['updatedBy'],
         "private_fields": ['updatedBy'],
         "elasticsearch_private_index_body": {
             "settings": CVISB_DEFAULT_SETTINGS,
