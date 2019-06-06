7d7b97797ee291123434e76b69e73d00f478b1eb
Laura Hughes <flaneuse@users.noreply.github.com>
2019-06-06 16:30:05 -0700

    + sample datasource; :crossed_fingers: table refreshes on data change

---
+++ sample-viewer/src/app/_services/samples.datasource.ts
@@ -0,0 +1,60 @@
+// based on https://blog.angular-university.io/angular-material-data-table/
+// For server-side filtering
+// HOWEVER-- server-side filtering doesn't really work, since the data need to be nested before they're filtered.
+// So relying still on client-side pagination/sorting
+// BUT-- need datasource so the table updates if the data changes (not cached)
+import { CollectionViewer, DataSource } from "@angular/cdk/collections";
+import { Observable, of, BehaviorSubject } from "rxjs";
+// import { ApiService } from "./api.service";
+import { GetSamplesService } from './get-samples.service';
+import { catchError, finalize } from "rxjs/operators";
+
+import { Sample, SampleWide } from '../_models';
+
+export class SamplesDataSource implements DataSource<SampleWide> {
+
+  private samplesSubject = new BehaviorSubject<SampleWide[]>([]);
+
+  // Loading spinner
+  private loadingSubject = new BehaviorSubject<boolean>(false);
+  public loadingState$ = this.loadingSubject.asObservable();
+
+  // Patient count
+  private resultCountSubject = new BehaviorSubject<number>(0);
+  public resultCountState$ = this.resultCountSubject.asObservable();
+
+  constructor(
+    private sampleSvc: GetSamplesService,
+  ) {
+
+  }
+
+  loadSamples(qParams) {
+    console.log('calling samples.dataSource:loadSamples')
+
+    this.loadingSubject.next(true);
+
+    this.sampleSvc.getSamples(qParams).pipe(
+      catchError(() => of([])),
+      finalize(() => this.loadingSubject.next(false))
+    )
+      .subscribe(sampleList => {
+        console.log('sample list from data source:')
+        console.log(sampleList)
+        this.resultCountSubject.next(sampleList.samples.length);
+        this.samplesSubject.next(sampleList.samples.length);
+      });
+
+  }
+
+  connect(collectionViewer: CollectionViewer): Observable<SampleWide[]> {
+    console.log("Connecting data source");
+    return this.samplesSubject.asObservable();
+  }
+
+  disconnect(collectionViewer: CollectionViewer): void {
+    this.samplesSubject.complete();
+    this.loadingSubject.complete();
+  }
+
+}
