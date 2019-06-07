6e2c8e87a87cecbcff0f2d8e5aa0fcf9a78600cb
Laura Hughes <flaneuse@users.noreply.github.com>
2019-06-06 17:12:49 -0700

    🤞fix sample datasource ref

--- sample-viewer/src/app/app.module.ts
+++ sample-viewer/src/app/app.module.ts
@@ -19,7 +19,7 @@ import { EncodeHttpParamsInterceptor } from './_models/encode-http-params-interc
 // Services
 import { MyHttpClient } from './_services/http-cookies.service';
 import { DatePipe } from '@angular/common';
-import { DatasetResolver, PatientsResolver, AllPatientsResolver, HlaResolver, SamplesResolver } from './_services/';
+import { DatasetResolver, PatientsResolver, AllPatientsResolver, HlaResolver, SamplesResolver, SamplesDataSource } from './_services/';
 
 import { Angulartics2Module } from 'angulartics2';
 import { Angulartics2GoogleAnalytics } from 'angulartics2/ga';
@@ -222,6 +222,7 @@ import { PatientFilesComponent } from './patient-page/patient-files/patient-file
     AllPatientsResolver,
     SamplesResolver,
     HlaResolver,
+    SampleDataSource
     {
       provide: HTTP_INTERCEPTORS,
       useClass: EncodeHttpParamsInterceptor,
