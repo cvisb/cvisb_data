7c881f494a7f1f18bce807b101fe4fc5ecc244c1
Laura Hughes <flaneuse@users.noreply.github.com>
2019-06-10 09:17:42 -0700

    get/use dataCatalog endpoint

--- sample-viewer/src/app/admin/about-data/about-data.component.ts
+++ sample-viewer/src/app/admin/about-data/about-data.component.ts
@@ -1,14 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
+import { GetDatacatalogService } from '../../_services';
+
 @Component({
   selector: 'app-about-data',
   templateUrl: './about-data.component.html',
   styleUrls: ['./about-data.component.scss']
 })
-export class AboutDataComponent implements OnInit {
-  dateModified = "2019-01-24";
 
-  constructor() { }
+export class AboutDataComponent implements OnInit {
+  dataModified: string;
+
+  constructor(private dataCatalogSvc: GetDatacatalogService) {
+    this.dataModified = this.dataCatalogSvc.dataModified;
+  }
 
   ngOnInit() {
   }
