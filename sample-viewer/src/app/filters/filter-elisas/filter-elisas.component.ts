46b4dd6f9e5d8c33bb23231d4243147b0e6bdae2
Laura Hughes <flaneuse@users.noreply.github.com>
2019-06-11 10:08:38 -0700

    Add multiselect for ELISA filter conditions

--- sample-viewer/src/app/filters/filter-elisas/filter-elisas.component.ts
+++ sample-viewer/src/app/filters/filter-elisas/filter-elisas.component.ts
@@ -19,22 +19,43 @@ export class FilterElisasComponent implements OnInit {
   results: string[] = ["positive", "negative", "unknown"];
   timepoints: string[] = ["patient admission", "survivor enrollment"];
 
-  elisaForm = this.fb.group({
-    virus: [[]],
-    assayType: [[]],
-    ELISAresult: [[]],
-    timepoint: [[]]
+  elisaForm: FormGroup = this.fb.group({
+    elisaGroups: this.fb.array([this.createGroup()])
   });
 
-
+  // expanded from https://alligator.io/angular/reactive-forms-formarray-dynamic-fields/
+  // and https://medium.com/aubergine-solutions/add-push-and-remove-form-fields-dynamically-to-formarray-with-reactive-forms-in-angular-acf61b4a2afe
   constructor(
     private fb: FormBuilder,
     private requestSvc: RequestParametersService) {
     this.elisaForm.valueChanges.subscribe(val => {
-      console.log(val)
-
-      this.requestSvc.updateParams(this.endpoint, { field: 'elisa', value: [val] });
+      this.requestSvc.updateParams(this.endpoint, { field: 'elisa', value: val.elisaGroups });
     })
+  }
+
+  ngOnInit() {
+  }
+
+
+  createGroup(): FormGroup {
+    return this.fb.group({
+      virus: [[]],
+      assayType: [[]],
+      ELISAresult: [[]],
+      timepoint: [[]],
+      connector: "and"
+    });
+  }
+
+  addELISA() {
+    this.elisaGroups = this.elisaForm.get('elisaGroups') as FormArray;
+    this.elisaGroups.push(this.createGroup());
+  }
+
+  deleteELISA($event, idx) {
+    this.elisaGroups = this.elisaForm.get('elisaGroups') as FormArray;
+    // Remove the elisa group from the array
+    this.elisaGroups.removeAt(idx);
   }
 
   ngOnInit() {
