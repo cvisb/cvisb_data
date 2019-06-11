d5d7393f0d722aadfcfc8380b32e9c049a53ab92
Laura Hughes <flaneuse@users.noreply.github.com>
2019-06-10 18:14:45 -0700

    troubleshooting multi cross endpoints

--- sample-viewer/src/app/download-btn/download-btn.component.ts
+++ sample-viewer/src/app/download-btn/download-btn.component.ts
@@ -45,7 +45,7 @@ export class DownloadBtnComponent implements OnInit {
     })
 
     requestSvc.patientParamsState$.subscribe((qParams: RequestParamArray) => {
-      this.qParams = this.requestSvc.reducePatientParams(qParams);
+      this.qParams = this.requestSvc.reducePatientParams(qParams[])[0];
       console.log(this.qParams)
     })
 
