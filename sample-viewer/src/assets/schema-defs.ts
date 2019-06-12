d654fec5a81f9659d42125e7bb4daf0f0424251a
Laura Hughes <flaneuse@users.noreply.github.com>
2019-06-12 12:59:58 -0700

    move schema defs to const file

--- sample-viewer/src/assets/schema-defs.ts
+++ sample-viewer/src/assets/schema-defs.ts
@@ -1,3 +1,4 @@
+// TODO: auto-generate from schema yaml files
 export abstract class SchemaDefs {
   static patientProperties: string[] = ["alternateIdentifier", "patientID", "cohort", "outcome", "infectionYear", "country.identifier", "gID", "sID"];
   static sampleProperties: string[] = ["sampleType", "location.lab", "species", "location.numAliquots", "sampleGroup"];
