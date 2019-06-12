06987558e7d5255ff391cbe9924efac0c30c72b3
Laura Hughes <flaneuse@users.noreply.github.com>
2019-06-12 15:51:10 -0700

    clean up console.logs

--- sample-viewer/src/app/_services/request-parameters.service.ts
+++ sample-viewer/src/app/_services/request-parameters.service.ts
@@ -157,9 +157,7 @@ export class RequestParametersService {
   }
 
   reduceSampleParams(request_params: RequestParamArray): HttpParams {
-    console.log(this.sampleProperties)
     let reduced = this.reduceParams(request_params, 'sample');
-    console.log(reduced)
 
     // default options
     // Note: * will only return those samples who are in the patient registry.  "" will return everything
@@ -243,10 +241,6 @@ export class RequestParametersService {
   }
 
   reduceParams2string(request_params: RequestParamArray, filterBy, qQuery?: boolean): string {
-    console.log(request_params)
-
-    console.log(request_params.filter(d => filterBy.includes(d.field) || !d.field))
-
     let params: RequestParamArray;
 
     if (qQuery) {
