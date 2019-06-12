4d1215c26c9af673d9defb706bb8759df41315c1
Laura Hughes <flaneuse@users.noreply.github.com>
2019-06-12 15:06:12 -0700

    fix .ts err

--- sample-viewer/src/app/sample/sample-table/sample-table.component.ts
+++ sample-viewer/src/app/sample/sample-table/sample-table.component.ts
@@ -56,7 +56,7 @@ export class SampleTableComponent implements OnInit {
 
 
   ngOnInit() {
-    this.dataSource = new SamplesDataSource(this.sampleSvc);
+    this.dataSource = new SamplesDataSource(this.sampleSvc, this.apiSvc);
     this.dataSource.loadSamples([], "", null, 0, 10);
     // this.dataSource.loadSamples(new HttpParams().set("q", "__all__"));
   }
