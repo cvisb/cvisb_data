6c8d1eb7001de49d7b3c0ba526258c00829a398c
Laura Hughes <flaneuse@users.noreply.github.com>
2019-06-28 10:38:25 -0700

    Update check_roster_coverage.py

--- sample-viewer-api/src/static/data/clean_patients/check_roster_coverage.py
+++ sample-viewer-api/src/static/data/clean_patients/check_roster_coverage.py
@@ -70,7 +70,6 @@ merged[merged.KGH_id]._merge.value_counts(dropna=False)
 # Count by source...
 merged['source2'] = merged.source.apply(helpers.listify)
 
-merged.source.value_counts()
 
 merged[(merged._merge == "left_only") & merged.KGH_id].source2.apply(lambda x: pd.Series([i for i in x])).melt().value.value_counts()
 
@@ -79,32 +78,68 @@ merged[(merged._merge == "left_only") & merged.KGH_id].source2.apply(lambda x: p
 cols2save = ['ID', 'allIDs', 'cohort_x', 'outcome_x', 'source']
 merged_copy = merged.loc[(merged._merge == "left_only") & merged.KGH_id, cols2save].copy()
 
+
+
 # all gIDs -- rename
 merged_copy.rename(columns={"ID": "gID", "cohort_x": "cohort", "outcome_x": "outcome"}, inplace = True)
 patient_file = "/Users/laurahughes/GitHub/cvisb_data/sample-viewer-api/src/static/data/output_data/patients/patients_2019-06-19_PRIVATE_ALL.json"
 
 patients = pd.read_json(patient_file, dtype={'hasPatientData': bool})
 
-
 # Merge back *all* the patients, to account for the patients in John's data who lack study specific IDs.
 patients.patientID.value_counts(dropna = False)
 
 # all gIDS; don't save sIDs
-patients = patients.loc[patients.patientID != patients.patientID, ['gID', 'cohort', 'outcome', 'hasPatientData']]
+patients_missing = patients.loc[patients.patientID != patients.patientID, ['gID', 'cohort', 'outcome', 'hasPatientData', 'source']]
 # luckily, all singles, so convert list to string
 # patients['gIDlength'] = patients.gID.apply(lambda x: len(x))
-patients['gID'] = patients.gID.apply(lambda x: x[0])
-
+patients_missing['gID'] = patients_missing.gID.apply(lambda x: x[0])
+patients_missing[patients_missing.duplicated(subset="gID")].gID
 
 
 merged_copy.cohort.apply(lambda x: len(x)).value_counts()
 
-patients.head()
+patients_missing.head()
 # all unique; can just concat
-testmerge = pd.merge(patients, merged_copy, on=["gID"], how="outer", indicator=True)
+testmerge = pd.merge(patients_missing, merged_copy, on=["gID"], how="outer", indicator=True)
 
-merged2save = pd.concat([patients, merged_copy])
+merged2save = pd.concat([patients_missing, merged_copy])
 merged2save.head()
 
 
-merged2save[["gID", "cohort", "outcome", "hasPatientData"]].sort_values(["hasPatientData", "gID"]).to_csv('/Users/laurahughes/GitHub/cvisb_data/sample-viewer-api/src/static/data/output_data/patients/inconsistencies/2019-06-19_allknownpatients_noPublicID.csv', index=False)
+merged2save[["gID", "cohort", "outcome", "hasPatientData", "source"]].sort_values(["hasPatientData", "gID"]).to_csv('/Users/laurahughes/GitHub/cvisb_data/sample-viewer-api/src/static/data/output_data/patients/inconsistencies/2019-06-19_allknownpatients_noPublicID.csv', index=False)
+
+
+x = pd.read_excel("../input_data/patient_rosters/survivor_IDdict_v2_2019-03-06_PRIVATE.xlsx", usecols=list(range(0,15)), converters={'Study Specific #': str, 'ID Number': str})
+x.head()
+ids = x.iloc[1772:1776]
+ids.head()
+
+ids['gID'] = ids["G-NUM"].apply(helpers.splitGID)
+
+
+
+# List from Emily, 20 June 2019
+em = pd.read_csv("/Users/laurahughes/GitHub/cvisb_data/sample-viewer-api/src/static/data/input_data/patient_rosters/unnamed originals/StudySpecificNumbersforLaura_20Jun19_EE.csv")
+# remove any blank rows
+em.dropna(subset=["gID"], axis=0, inplace=True)
+em.head()
+
+# remove anything which are straight duplicates-- my fault, since the patients list from John included duplicate IDs.
+em.drop_duplicates(subset=["gID", "Study Specific #"], inplace=True)
+# Export contradictory IDs.
+em[em.duplicated(subset=["Study Specific #"], keep=False)].sort_values("Study Specific #").to_csv('/Users/laurahughes/GitHub/cvisb_data/sample-viewer-api/src/static/data/output_data/patients/inconsistencies/2019-06-29_StudySpecificNumbers_duplicates.csv', index=False)
+
+# testing whether all ids are unique, comparing w/ previous list of IDs.
+p = patients.loc[patients.patientID == patients.patientID, ['patientID', 'sID', 'gID']]
+
+p['ID'] = p.patientID.apply(lambda x: x[1:])
+
+e = em.drop_duplicates(subset="Study Specific #")
+
+
+em_id_check = pd.merge(e, p, how="outer", indicator=True, left_on="Study Specific #", right_on="ID")
+
+em_id_check.sample(6)
+
+em_id_check._merge.value_counts()
