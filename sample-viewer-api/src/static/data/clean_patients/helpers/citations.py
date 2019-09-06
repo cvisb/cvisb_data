c80663c5ae0839a339f7f4f37d9b89daa8929aa2
Laura Hughes <flaneuse@users.noreply.github.com>
2019-09-06 10:14:58 -0700

    update citations to match schema.org

--- sample-viewer-api/src/static/data/clean_patients/helpers/citations.py
+++ sample-viewer-api/src/static/data/clean_patients/helpers/citations.py
@@ -1,19 +1,55 @@
 ncbi_stub = 'https://api.ncbi.nlm.nih.gov/lit/ctxp/v1/pubmed/?format=csl&id='
 # example: 'https://api.ncbi.nlm.nih.gov/lit/ctxp/v1/pubmed/?format=csl&id=26276630'
 import requests
+import numpy as np
 
+
+def dateArr2Str(arr):
+    return(f"{str(arr[0])}-{str(arr[1]).zfill(2)}-{str(arr[2]).zfill(2)}")
+
+def convertAuthor(authorObj):
+    try:
+        given = authorObj['given']
+    except:
+        given = np.nan
+    try:
+        family = authorObj['family']
+    except:
+        family = np.nan
+    return({'givenName': given,
+    'familyName': family
+    })
 
 def getCitation(pmid, ncbi_stub=ncbi_stub):
     res = requests.get(ncbi_stub + str(pmid))
     if(res.status_code == 200):
-        citation = res.json()
-        citation['url'] = 'https://www.ncbi.nlm.nih.gov/pubmed/?term=' + \
-            citation['PMID']
+        citation_raw = res.json()
+        citation = {}
+        # reformat to schema.org format.
+        citation['doi'] = citation_raw['DOI']
+        citation['pmid'] = citation_raw['PMID']
+        citation["identifier"] = citation_raw['id']
+        citation["issn"] = citation_raw['ISSN']
+
+        citation["author"] = [convertAuthor(author) for author in citation_raw['author']]
+        citation["datePublished"] = dateArr2Str(citation_raw['issued']['date-parts'][0])
+
+        citation["issueNumber"] = citation_raw['issue']
+        citation["journalName"] = citation_raw['container-title']
+        citation["journalNameAbbrev"] = citation_raw['container-title-short']
+        citation["name"] = citation_raw['title']
+
+        citation["pagination"] = citation_raw['page']
+
+        citation["volumeNumber"] = citation_raw['volume']
+        citation['url'] =  'https://www.ncbi.nlm.nih.gov/pubmed/?term=' + \
+            citation['pmid']
         return(citation)
 
 # getCitation("26276630")
 
 
+getCitation("26276630")
 # Publisher Institution
 cvisb = {
     "url": "https://cvisb.org/",
