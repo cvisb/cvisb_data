f9c3ce61fd1a49ee0fc99571797ccf9a7ef31569
Laura Hughes <flaneuse@users.noreply.github.com>
2019-09-09 09:54:03 -0700

    remove console.logs

--- sample-viewer/src/app/_services/anchor.service.ts
+++ sample-viewer/src/app/_services/anchor.service.ts
@@ -12,11 +12,9 @@ export class AnchorService {
   constructor(private stripPipe: StripWhitespacePipe) { }
 
   clickAnchor(anchor_tag: string) {
-    console.log(anchor_tag)
     anchor_tag = this.stripPipe.transform(anchor_tag);
 
     let anchor_div = document.querySelector("#" + anchor_tag);
-    console.log(anchor_div)
     if (anchor_div) {
       anchor_div.scrollIntoView();
     }
