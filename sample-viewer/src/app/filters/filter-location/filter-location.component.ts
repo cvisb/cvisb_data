e7d176915aa487cb7d2d34cf36b1768588a24a63
Laura Hughes <flaneuse@users.noreply.github.com>
2019-09-10 17:40:03 -0700

    test: revamp country filters like expts

--- sample-viewer/src/app/filters/filter-location/filter-location.component.ts
+++ sample-viewer/src/app/filters/filter-location/filter-location.component.ts
@@ -1,7 +1,6 @@
-import { Component, OnChanges, Input } from '@angular/core';
+import { Component, OnChanges, OnInit, Input } from '@angular/core';
 
 import { RequestParametersService } from '../../_services';
-import { RequestParam } from '../../_models';
 
 @Component({
   selector: 'app-filter-location',
@@ -17,6 +16,22 @@ export class FilterLocationComponent implements OnChanges {
   constructor(private requestSvc: RequestParametersService) {
   }
 
+  ngOnInit() {
+    switch (this.endpoint) {
+      case "patient":
+        this.requestSvc.patientParamsState$.subscribe(params => {
+          this.updateCountrySelections(params);
+        })
+        break;
+
+      case "sample":
+        this.requestSvc.sampleParamsState$.subscribe(params => {
+          this.updateCountrySelections(params);
+        })
+        break;
+    }
+  }
+
   ngOnChanges() {
     // console.log(this.countries);
     if (this.countries && this.countries.length > 0) {
@@ -25,20 +40,40 @@ export class FilterLocationComponent implements OnChanges {
 
   }
 
+  updateCountrySelections(params, fieldName = "country.identifier") {
+    let filtered = params.filter(d => d.field === fieldName);
+
+    let selectedCountries = filtered.flatMap((d: any) => d.value);
+
+    this.countries.forEach((d: any) => {
+      d['disabled'] = selectedCountries.includes(d.term);
+    })
+
+
+    console.log(this.countries);
+
+  }
+
   selectCountry(ctry: string) {
-    this.requestSvc.updateParams(this.endpoint, { field: 'country.identifier', value: ctry })
+    let idx = this.countries.findIndex(d => d.term === ctry);
+    this.countries[idx].disabled = !this.countries[idx].disabled;
+
+    let countries = this.countries.filter(d => !d.disabled).map(d => d.name);
+    console.log(countries);
+
+    this.requestSvc.updateParams(this.endpoint, { field: 'country.identifier', value: countries })
   }
 
   addMissing() {
     let keys = this.countries.map(d => d.term);
 
 
-    // console.log(this.all_countries)
+    console.log(this.all_countries)
     if (this.all_countries) {
       let missing_data = this.all_countries.filter(d => !keys.includes(d.term));
 
       missing_data.forEach(d => {
-        this.countries.push({ key: d.term, name: d.name, value: 0 });
+        this.countries.push({ key: d.term, name: d.name, value: 0, disabled: true });
       })
     }
   }
