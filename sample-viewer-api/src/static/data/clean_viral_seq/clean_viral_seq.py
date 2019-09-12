8b75f6c4f89ccc03c7c59e873b8938a245958dea
Laura Hughes <flaneuse@users.noreply.github.com>
2019-09-12 11:07:07 -0700

    remove duplicate patients, samples

--- sample-viewer-api/src/static/data/clean_viral_seq/clean_viral_seq.py
+++ sample-viewer-api/src/static/data/clean_viral_seq/clean_viral_seq.py
@@ -37,10 +37,12 @@ patientCols = ["patientID", "alternateIdentifier", "hasPatientData", "hasSurvivo
                "dateModified", "updatedBy", "cohort", "outcome", "country", "infectionYear", "species"]
 
 # For all experiments, to relate sample <--> experiment
-sampleCols = ["creatorInitials", "sampleLabel", "sampleType", "species", "sampleID", "samplingDate"]
+sampleCols = ["creatorInitials", "sampleLabel",
+              "sampleType", "species", "sampleID", "samplingDate"]
 
 # For all experiments with accession numbers, generate data downloads
-downloadCols = ["name", "includedInDataset", "identifier", "contentUrl", "additionalType", "measurementTechnique", "dateModified", "experimentIDs", "contentUrlRepository", "contentUrlIdentifier", "citation", "updatedBy"]
+downloadCols = ["name", "includedInDataset", "identifier", "contentUrl", "additionalType", "measurementTechnique",
+                "dateModified", "experimentIDs", "contentUrlRepository", "contentUrlIdentifier", "citation", "updatedBy"]
 
 # keep as np.nan if want to use today's date
 dateModified = "2019-09-04"
@@ -79,14 +81,19 @@ def getDNAseq(all_seq, df, df_id, virus, seq_type="DNAsequence", segment=pd.np.n
     for seq in all_seq:
         id = seq.id.split("|")[0]
         if(sum(df[df_id] == id) == 0):
-            log_notes.append(f"no patient found in sequence metadata file for sequence {seq.id}")
-        seq_obj = [{seq_type: str(seq.seq).upper(), "virus": virus, "segment": segment}]
+            log_notes.append(
+                f"no patient found in sequence metadata file for sequence {seq.id}")
+            log_notes.append("*" * 100)
+        seq_obj = [{seq_type: str(seq.seq).upper(),
+                    "virus": virus, "segment": segment}]
         df.at[df[df_id] == id, 'data'] = seq_obj
     return(df)
+
 
 def getAltIDs(row):
     ids = list(set([row.patientID, row.privatePatientID]))
     return(ids)
+
 
 def getPublicID(row):
     if(row.patientID == row.patientID):
@@ -101,23 +108,28 @@ def getPublicID(row):
 # HOWEVER-- while this is required for KGH ids to get back to a patient id--
 # it doesn't work for other places, since they repeat the same patient stub for different patients.
 # so sticking with the original (verbose) id
+
+
 def getPrivateID(row):
     if(row.KGH_id):
         return(helpers.interpretID(row.rawID))
     else:
-        dupe_id = re.search("(.+)(\.\d)(.+)",row.id)
+        dupe_id = re.search("(.+)(\.\d)(.+)", row.id)
         # Assuming "id.2" is the same person as "id"
         if(dupe_id):
             return(dupe_id[1] + dupe_id[3])
         return(row.id)
 
 # Experiment IDs should be unique... so adding back in .2 if it's a longitudinal sample.
-def getExptID(row, expt_stub = "LASV_seq_"):
-    dupe_id = re.search("(.+)(\.\d)(.+)",row.id)
+
+
+def getExptID(row, expt_stub="LASV_seq_"):
+    dupe_id = re.search("(.+)(\.\d)(.+)", row.id)
     # Assuming "id.2" is the same person as "id"
     if(dupe_id):
         return(expt_stub + row.patientID + dupe_id[2])
     return(expt_stub + row.patientID)
+
 
 # initialize log file
 global log_notes
@@ -137,21 +149,25 @@ log_notes.append("Starting cleaning of Lassa viral sequences")
 lsv = pd.read_csv(lassa_MDfile)
 lsv.head()
 lsv["rawID"] = lsv.id.apply(lambda x: "_".join(x.split("_")[0:-2]))
-lsv['KGH_id'] = lsv.rawID.apply(helpers.checkIDstructure).apply(lambda x: not x)
-lsv['privatePatientID'] = lsv.apply(getPrivateID, axis = 1)
+lsv['KGH_id'] = lsv.rawID.apply(
+    helpers.checkIDstructure).apply(lambda x: not x)
+lsv['privatePatientID'] = lsv.apply(getPrivateID, axis=1)
 
 
 # Check for duplicates
 num_dupes = sum(lsv.duplicated(subset="privatePatientID", keep=False))
 if(num_dupes > 0):
-    log_notes.append(f"{num_dupes} duplicate privatePatientIDs found in viral sequences:")
-    log_notes.append(lsv.loc[lsv.duplicated(subset="privatePatientID", keep=False), ['id', 'privatePatientID']].sort_values("privatePatientID"))
-    log_notes.append("*"*100)
+    log_notes.append(
+        f"{num_dupes} duplicate privatePatientIDs found in viral sequences:")
+    log_notes.append(lsv.loc[lsv.duplicated(subset="privatePatientID", keep=False), [
+                     'id', 'privatePatientID']].sort_values("privatePatientID"))
+    log_notes.append("*" * 100)
 num_dupes = sum(lsv.duplicated(subset="id", keep=False))
 if(num_dupes > 0):
     log_notes.append(f"{num_dupes} duplicate ids found in viral sequences:")
-    log_notes.append(lsv.loc[lsv.duplicated(subset="id", keep=False), ['id', 'privatePatientID']].sort_values("privatePatientID"))
-    log_notes.append("*"*100)
+    log_notes.append(lsv.loc[lsv.duplicated(subset="id", keep=False), [
+                     'id', 'privatePatientID']].sort_values("privatePatientID"))
+    log_notes.append("*" * 100)
 
 lsv['outcome_copy'] = lsv.outcome
 lsv['outcome'] = lsv.outcome.apply(helpers.cleanOutcome)
@@ -162,29 +178,34 @@ lsv['cohort'] = "Lassa"
 # [Merge in ids]  ----------------------------------------------------------------------------------------------------
 # Merge in the known set of ids...
 lsv = pd.merge(lsv, ids[["ID", "patientID"]], how="left", indicator=True,
-# lsv = pd.merge(lsv, ids[["ID", "patientID", "outcome", "cohort", "countryName"]], how="left", indicator=True,
-                 right_on="ID", left_on="privatePatientID")
+               # lsv = pd.merge(lsv, ids[["ID", "patientID", "outcome", "cohort", "countryName"]], how="left", indicator=True,
+               right_on="ID", left_on="privatePatientID")
 
 lsv.groupby('KGH_id')._merge.value_counts()
 
 num_missing_pubIDs = sum((lsv.KGH_id) & (lsv._merge == "left_only"))
 if(num_missing_pubIDs > 0):
-    log_notes.append(f"{num_missing_pubIDs} KGH-esque IDs are missing public IDs in the master KGH roster:")
-    log_notes.append(lsv[(lsv.KGH_id) & (lsv._merge == "left_only")][['privatePatientID', 'id']])
-    log_notes.append("*"*100)
+    log_notes.append(
+        f"{num_missing_pubIDs} KGH-esque IDs are missing public IDs in the master KGH roster:")
+    log_notes.append(lsv[(lsv.KGH_id) & (lsv._merge == "left_only")][[
+                     'privatePatientID', 'id']])
+    log_notes.append("*" * 100)
     log_notes.append("\n")
 
-lsv['patientID'] = lsv.apply(getPublicID, axis = 1)
+lsv['patientID'] = lsv.apply(getPublicID, axis=1)
 # Sequence ID in the files is publicID_countryiso3_year
+
+
 def getSeqID(row):
     if(row.KGH_id):
         return(f"{row.patientID}_{row.country_iso3}_{row.year}")
     else:
         return(row.id)
 
-lsv['seqID'] = lsv.apply(getSeqID, axis = 1)
 
-lsv['alternateIdentifier'] = lsv.apply(getAltIDs, axis = 1)
+lsv['seqID'] = lsv.apply(getSeqID, axis=1)
+
+lsv['alternateIdentifier'] = lsv.apply(getAltIDs, axis=1)
 
 # lsv['mergeIssue'] = None
 # lsv = helpers.checkMerge2(lsv,
@@ -198,7 +219,6 @@ lsv['alternateIdentifier'] = lsv.apply(getAltIDs, axis = 1)
 # lsv.loc[lsv.KGH_id, ['ID', 'privatePatientID', 'patientID', 'outcome_x', 'outcome_y']]
 # sum((lsv_data.countryName_x != lsv_data.countryName_y) &
 #     (lsv_data.countryName_y == lsv_data.countryName_y))
-
 
 
 lsv['inAlignment'] = lsv.curated
@@ -219,7 +239,8 @@ lsv.groupby("host").species.value_counts(dropna=False)
 lsv_data_seq = list(SeqIO.parse(lassaS_Rawfile, "fasta"))
 lsv_data = lsv.copy(deep=True)
 lsv_data['data'] = None
-lsv_data = getDNAseq(lsv_data_seq, lsv_data, 'seqID', "Lassa", "DNAsequence", "S")
+lsv_data = getDNAseq(lsv_data_seq, lsv_data, 'seqID',
+                     "Lassa", "DNAsequence", "S")
 
 # Remove anything without any seq data
 lsv_data = lsv_data[lsv_data.data.apply(lambda x: x is not None)]
@@ -228,9 +249,11 @@ lsv_data['experimentID'] = lsv_data.apply(getExptID, axis=1)
 
 num_dupes_expt = sum(lsv_data.duplicated(subset="experimentID", keep=False))
 if(num_dupes_expt > 0):
-    log_notes.append(f"{num_dupes_expt} duplicate experimentIDs found in viral sequences:")
-    log_notes.append(lsv_data.loc[lsv_data.duplicated(subset="experimentID", keep=False), ['id', 'privatePatientID', "experimentID"]].sort_values("privatePatientID"))
-    log_notes.append("*"*100)
+    log_notes.append(
+        f"{num_dupes_expt} duplicate experimentIDs found in viral sequences:")
+    log_notes.append(lsv_data.loc[lsv_data.duplicated(subset="experimentID", keep=False), [
+                     'id', 'privatePatientID', "experimentID"]].sort_values("privatePatientID"))
+    log_notes.append("*" * 100)
 
 lsv_data['sampleLabel'] = lsv_data.patientID
 lsv_data['genbankID'] = lsv_data.accession_S
@@ -309,41 +332,85 @@ expts['hasSurvivorData'] = False
 # sample-specific properties
 expts['sampleType'] = "viralRNA"
 expts['creatorInitials'] = f"{(updatedBy.split(' ')[0][0] + updatedBy.split(' ')[1][0]).lower()}"
-expts['sampleID'] = expts.apply(lambda x: f"{x.creatorInitials}-{x.sampleLabel}_{x.sampleType}", axis = 1)
-
-# Make sure arrays are arrays
-expts['data'] = expts.data.apply(helpers.listify)
-expts['citation'] = expts.citation.apply(helpers.listify)
-expts['_source'] = source
+expts['sampleID'] = expts.apply(
+    lambda x: f"{x.creatorInitials}-{x.sampleLabel}_{x.sampleType}", axis=1)
 
 # datadownload-specific properties
 idVars = list(set(expts.columns) - set(["accession_L", "accession_S"]))
-dwnlds = pd.melt(expts, id_vars=idVars, value_vars=["accession_L", "accession_S"], value_name="accession")
+dwnlds = pd.melt(expts, id_vars=idVars, value_vars=[
+                 "accession_L", "accession_S"], value_name="accession")
 dwnlds.dropna(subset=["accession"], axis=0, inplace=True)
 
-dwnlds['name'] = dwnlds.apply(lambda x: x.patientID + "_" + x.accession, axis=1)
-dwnlds['identifier'] = dwnlds.apply(lambda x: x.patientID + "_" + x.accession, axis=1)
-dwnlds['contentUrl'] = dwnlds.apply(lambda x: "https://www.ncbi.nlm.nih.gov/nuccore/" + x.accession, axis=1)
+dwnlds['name'] = dwnlds.apply(
+    lambda x: x.patientID + "_" + x.accession, axis=1)
+dwnlds['identifier'] = dwnlds.apply(
+    lambda x: x.patientID + "_" + x.accession, axis=1)
+dwnlds['contentUrl'] = dwnlds.apply(
+    lambda x: "https://www.ncbi.nlm.nih.gov/nuccore/" + x.accession, axis=1)
 dwnlds['includedInDataset'] = 'viralseq'
 dwnlds['additionalType'] = 'raw data'
 dwnlds['experimentIDs'] = dwnlds.experimentID.apply(lambda x: [x])
 dwnlds['contentUrlRepository'] = "GenBank"
 dwnlds['contentUrlIdentifier'] = dwnlds.accession
 
+# Make sure arrays are arrays
+expts['data'] = expts.data.apply(helpers.listify)
+expts['citation'] = expts.citation.apply(helpers.listify)
+expts['_source'] = source
+
+# samples
+all_samples = expts[sampleCols]
+
+all_samples.head()
+sampleDupeCols = ["sampleID"]
+samples = all_samples.drop_duplicates(subset=sampleDupeCols)
+dupe_removed = len(all_samples) - len(samples)
+if(dupe_removed > 0):
+    log_notes.append(f"{dupe_removed} duplicate samples were removed:")
+    dupe_samples = all_samples[all_samples.duplicated(subset=sampleDupeCols)]
+    log_notes.append(dupe_samples.sampleID)
+    log_notes.append("*" * 100)
+
+# patients
+# Remove duplicate patients
+all_patients = expts.loc[~expts.KGH_id, patientCols]
+
+# Can't check for any columns that are lists... but should be the same.
+patientDupeCols = ["patientID", "hasPatientData", "hasSurvivorData",
+                   "dateModified", "updatedBy", "cohort", "outcome", "infectionYear", "species"]
+patients = all_patients.drop_duplicates(subset=patientDupeCols)
+dupe_removed = len(all_patients) - len(patients)
+dupe_removed
+if(dupe_removed > 0):
+    log_notes.append(f"{dupe_removed} duplicate patients were removed:")
+    dupe_patients = all_patients[all_patients.duplicated(
+        subset=patientDupeCols)]
+    log_notes.append(dupe_patients.patientID)
+    log_notes.append("*" * 100)
+
+
 # [Export]  ----------------------------------------------------------------------------------------------------
 # Experiments
-expts[exptCols].to_json(f"{output_dir}/experiments/viral_seq_experiments_{today}.json", orient="records")
+expts[exptCols].to_json(
+    f"{output_dir}/experiments/viral_seq_experiments_{today}.json", orient="records")
+
+
 # patients
-expts.loc[~expts.KGH_id, patientCols].to_json(f"{output_dir}/patients/viral_seq_patients_{today}.json", orient="records")
+patients.to_json(
+    f"{output_dir}/patients/viral_seq_patients_{today}.json", orient="records")
 # samples
-expts[sampleCols].to_json(f"{output_dir}/samples/viral_seq_samples_{today}.json", orient="records")
+samples.to_json(
+    f"{output_dir}/samples/viral_seq_samples_{today}.json", orient="records")
 # data downloads
 # Make sure only to get the experiment IDs of those in the alignment
-expts.loc[expts.inAlignment,['seqType', 'experimentID']].groupby('seqType').experimentID.apply(list).to_json(f"{output_dir}/datadownloads/viral_seq_exptIDs_{today}.json")
-dwnlds[downloadCols].to_json(f"{output_dir}/datadownloads/viral_seq_datadownloads_{today}.json", orient="records")
+expts.loc[expts.inAlignment, ['seqType', 'experimentID']].groupby('seqType').experimentID.apply(
+    list).to_json(f"{output_dir}/datadownloads/viral_seq_exptIDs_{today}.json")
+dwnlds[downloadCols].to_json(
+    f"{output_dir}/datadownloads/viral_seq_datadownloads_{today}.json", orient="records")
 
 # datasets
-dwnlds.identifier.to_json(f"{output_dir}/datasets/viral_seq_accession_downloadIDs_{today}.json", orient="records")
+dwnlds.identifier.to_json(
+    f"{output_dir}/datasets/viral_seq_accession_downloadIDs_{today}.json", orient="records")
 
 # Write log file
 with open(f"{log_dir}/{today}_viral_sequence_cleanup.log", 'w') as f:
