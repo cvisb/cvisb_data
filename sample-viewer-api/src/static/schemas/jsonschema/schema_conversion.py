bfdf324d3da30993e1593e185e6921089ee8845a
Laura Hughes <flaneuse@users.noreply.github.com>
2019-09-12 13:43:35 -0700

    Update schema_conversion.py

--- sample-viewer-api/src/static/schemas/jsonschema/schema_conversion.py
+++ sample-viewer-api/src/static/schemas/jsonschema/schema_conversion.py
@@ -217,7 +217,7 @@ def main(args):
             if entity_dict['is_root']:
                 log_msg(error_file, "Opening file {} for root entity".format(entity_dict["file"]), verbose=args.verbose)
                 unauthorized_fields = process_file(entity=entity, entity_dict=preprocess_dict, error_file=error_file, _out_directory=_out_directory, args=args, namespace='')
-                sorted(unauthorized_fields)
+                unauthorized_fields = sorted(unauthorized_fields)
                 with open(_auth_out, 'a') as authfile:
                     authfile.write("*"*80 + "\nPublic fields for '{e}' entity: '{f}'\n".format(e=entity, f=list(unauthorized_fields)) + "*"*80 + '\n\n')
 
