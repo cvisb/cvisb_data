7f42e98dd9d464b4bb3cd5157a8db249b777f174
Laura Hughes <flaneuse@users.noreply.github.com>
2019-09-25 10:27:39 -0700

    remove release notes

--- sample-viewer/src/app/_services/get-datacatalog.service.ts
+++ sample-viewer/src/app/_services/get-datacatalog.service.ts
@@ -13,35 +13,7 @@ export class GetDatacatalogService {
   dataModified: string;
   dataCatalog: Object[];
   cvisbCatalog: Object;
-  releaseNotes: ReleaseNote[] = [
-    {
-      datePublished: "2019-09-15",
-      version: "0.1.0",
-      description: [
-        {
-          endpoint: "Data",
-          description: ["Initial publication of HLA data",
-            "Initial publication of aligned Lassa viral sequencing data"
-          ]
-        },
-        {
-          endpoint: "Patient metadata",
-          description: [
-            "Added Lassa acute patients and metadata",
-            "Added Ebola survivor patients and metadata",
-            "Added Lassa survivor patients basic data"
-          ]
-        },
-        {
-          endpoint: "Samples and sample metadata",
-          description: [
-            "Added samples from June/July 2018 trip to Kenema Government Hospital in Sierra Leone",
-            "Added samples from January 2019 trip to Kenema Government Hospital in Sierra Leone"
-          ]
-        },
-      ]
-    }
-  ];
+  releaseNotes: ReleaseNote[];
 
   constructor(private apiSvc: ApiService) {
 
@@ -49,9 +21,13 @@ export class GetDatacatalogService {
       this.dataCatalog = res['hits'];
       this.cvisbCatalog = this.dataCatalog.filter(d => d['identifier'] === "https://data.cvisb.org/")[0];
       this.dataModified = this.cvisbCatalog['dateModified'];
+
+      this.releaseNotes = this.cvisbCatalog['releaseNotes'];
+
+      this.releaseNotes.sort((a, b) => a.version > b.version ? -1 : 1);
     })
 
-    this.releaseNotes.sort((a, b) => a.version > b.version ? -1 : 1);
+
 
   }
 }
