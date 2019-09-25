d47a4d39c8d9282ca0934f04a9c1ab17b4712e4d
Laura Hughes <flaneuse@users.noreply.github.com>
2019-09-25 11:17:13 -0700

    Update get-datacatalog.service.ts

--- sample-viewer/src/app/_services/get-datacatalog.service.ts
+++ sample-viewer/src/app/_services/get-datacatalog.service.ts
@@ -22,6 +22,7 @@ export class GetDatacatalogService {
       let catalog = this.dataCatalog.filter(d => d['identifier'] === "https://data.cvisb.org/");
 
       if (catalog && catalog.length > 0){
+        console.log(catalog)
         this.cvisbCatalog = catalog[0];
 
         this.dataModified = this.cvisbCatalog['dateModified'];
