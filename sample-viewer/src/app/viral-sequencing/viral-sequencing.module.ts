c9770b6ceca0c02d5bce0b4fab04cc4fee17ffea
Laura Hughes <flaneuse@users.noreply.github.com>
2019-10-02 10:28:21 -0700

    update sources

--- sample-viewer/src/app/viral-sequencing/viral-sequencing.module.ts
+++ sample-viewer/src/app/viral-sequencing/viral-sequencing.module.ts
@@ -5,17 +5,17 @@ import { ViralSequencingRoutingModule } from './viral-sequencing-routing.module'
 
 // --- components ---
 import { ViralSequencingComponent } from './viral-sequencing.component';
-// import { AlignedSequencesComponent } from './aligned-sequences/aligned-sequences.component';
-// import { AlignedSequencesFiltersComponent } from './aligned-sequences-filters/aligned-sequences-filters.component';
-// import { SvgIconComponent } from '../svg-icon/svg-icon.component';
+import { AlignedSequencesComponent } from './aligned-sequences/aligned-sequences.component';
+import { AlignedSequencesFiltersComponent } from './aligned-sequences-filters/aligned-sequences-filters.component';
+import { SvgIconModule } from '../svg-icon/svg-icon.module';
 
 // -- Modules --
-// import { MaterialModule } from '../material.module';
+import { MaterialModule } from '../material.module';
 import { DatasetPageModule } from '../dataset-page/dataset-page.module';
-// import { NgSelectModule } from '@ng-select/ng-select';
-// import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-// import { DownloadAlignedSequencesComponent } from './download-aligned-sequences/download-aligned-sequences.component';
-// import { CompareCohortsComponent } from './compare-cohorts/compare-cohorts.component'
+import { NgSelectModule } from '@ng-select/ng-select';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { DownloadAlignedSequencesComponent } from './download-aligned-sequences/download-aligned-sequences.component';
+import { CompareCohortsComponent } from './compare-cohorts/compare-cohorts.component'
 
 
 // --- services ---
@@ -25,17 +25,27 @@ import { DatasetResolver } from '../_services/get-datasets.resolver';
 @NgModule({
   imports: [
     CommonModule,
-    // MaterialModule,
+    MaterialModule,
     ViralSequencingRoutingModule,
-    DatasetPageModule
+    DatasetPageModule,
+    SvgIconModule,
+    NgSelectModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   declarations: [
-    ViralSequencingComponent
-    // AlignedSequencesComponent,
-    // AlignedSequencesFiltersComponent,
-    // SvgIconComponent,
-    // DownloadAlignedSequencesComponent,
-    // CompareCohortsComponent
+    ViralSequencingComponent,
+    AlignedSequencesComponent,
+    AlignedSequencesFiltersComponent,
+    DownloadAlignedSequencesComponent,
+    CompareCohortsComponent
+  ],
+  exports: [
+    ViralSequencingComponent,
+    AlignedSequencesComponent,
+    AlignedSequencesFiltersComponent,
+    DownloadAlignedSequencesComponent,
+    CompareCohortsComponent
   ],
   providers: [
     MyHttpClient,
