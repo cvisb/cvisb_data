4528342d8d3aaa05dbeaa8441aa52ebcb9d9e67a
Laura Hughes <flaneuse@users.noreply.github.com>
2019-10-02 17:27:40 -0700

    Update viral-sequencing.module.ts

--- sample-viewer/src/app/viral-sequencing/viral-sequencing.module.ts
+++ sample-viewer/src/app/viral-sequencing/viral-sequencing.module.ts
@@ -5,8 +5,8 @@ import { ViralSequencingRoutingModule } from './viral-sequencing-routing.module'
 
 // --- components ---
 import { ViralSequencingComponent } from './viral-sequencing.component';
-import { AlignedSequencesComponent } from './aligned-sequences/aligned-sequences.component';
-import { AlignedSequencesFiltersComponent } from './aligned-sequences-filters/aligned-sequences-filters.component';
+// import { AlignedSequencesComponent } from './aligned-sequences/aligned-sequences.component';
+// import { AlignedSequencesFiltersComponent } from './aligned-sequences-filters/aligned-sequences-filters.component';
 import { SvgIconModule } from '../svg-icon/svg-icon.module';
 
 // -- Modules --
@@ -14,8 +14,8 @@ import { MaterialModule } from '../material.module';
 import { DatasetPageModule } from '../dataset-page/dataset-page.module';
 import { NgSelectModule } from '@ng-select/ng-select';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { DownloadAlignedSequencesComponent } from './download-aligned-sequences/download-aligned-sequences.component';
-import { CompareCohortsComponent } from './compare-cohorts/compare-cohorts.component'
+// import { DownloadAlignedSequencesComponent } from './download-aligned-sequences/download-aligned-sequences.component';
+// import { CompareCohortsComponent } from './compare-cohorts/compare-cohorts.component'
 
 
 // --- services ---
@@ -35,17 +35,17 @@ import { DatasetResolver } from '../_services/get-datasets.resolver';
   ],
   declarations: [
     ViralSequencingComponent,
-    AlignedSequencesComponent,
-    AlignedSequencesFiltersComponent,
-    DownloadAlignedSequencesComponent,
-    CompareCohortsComponent
+    // AlignedSequencesComponent,
+    // AlignedSequencesFiltersComponent,
+    // DownloadAlignedSequencesComponent,
+    // CompareCohortsComponent
   ],
   exports: [
     ViralSequencingComponent,
-    AlignedSequencesComponent,
-    AlignedSequencesFiltersComponent,
-    DownloadAlignedSequencesComponent,
-    CompareCohortsComponent
+    // AlignedSequencesComponent,
+    // AlignedSequencesFiltersComponent,
+    // DownloadAlignedSequencesComponent,
+    // CompareCohortsComponent
   ],
   providers: [
     MyHttpClient,
