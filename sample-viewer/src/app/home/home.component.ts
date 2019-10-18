66d3fa7bc3ce43912b97bd95f80a50678c937796
Laura Hughes <flaneuse@users.noreply.github.com>
2019-10-18 13:29:41 -0700

    forkjoining

--- sample-viewer/src/app/home/home.component.ts
+++ sample-viewer/src/app/home/home.component.ts
@@ -6,6 +6,9 @@ import { ActivatedRoute } from '@angular/router';
 
 import { HttpParams } from '@angular/common/http';
 
+import { forkJoin } from 'rxjs';
+import { map } from 'rxjs/operators';
+
 import { environment } from '../../environments/environment';
 
 import { latLng, tileLayer } from 'leaflet';
@@ -60,58 +63,115 @@ export class HomeComponent implements OnInit {
 
   ngOnInit() {
     if (isPlatformBrowser(this.platformId)) {
-      let patientParams = new HttpParams().set("q", "__all__");
-      let transitionSync = d3.transition().duration(5000);
-
-      this.apiSvc.get("patient", patientParams, 0).subscribe(res => {
-        this.patientCount = res['total'];
-
-        let patientDiv = d3.selectAll("#patient").selectAll(".count-value");
-
-        patientDiv.transition(transitionSync)
-          // .duration(transitionTime)
-          .tween("text", function(_) {
-            let countMax = this['textContent'];
-            var i = <any>d3.interpolate(0, countMax);
-            return function(t) {
-              d3.select(this).text(Math.round(i(t)));
-            };
-          });
-      });
-
-      this.apiSvc.get("sample", patientParams, 0).subscribe(res => {
-        this.sampleCount = res['total'];
-
-        let sampleDiv = d3.select("#sample").selectAll(".count-value");
-
-        sampleDiv.transition(transitionSync)
-          // .duration(transitionTime * (925/5039))
-          .tween("text", function(_) {
-            let countMax = this['textContent'];
-            var i = <any>d3.interpolate(0, countMax);
-            return function(t) {
-              d3.select(this).text(Math.round(i(t)));
-            };
-          });
-      });
-
-      this.exptSvc.getExptCounts().subscribe(res => {
-        this.experimentCount = res;
-      })
-
-      let dataDiv = d3.selectAll("#dataset").selectAll(".count-value");
-
-      dataDiv.transition(transitionSync)
-        // .duration(transitionTime * (312/5039))
-        .tween("text", function(_) {
-          let countMax = this['textContent'];
-          var i = <any>d3.interpolate(0, countMax);
-          return function(t) {
-            d3.select(this).text(Math.round(i(t)));
-          };
-        });
-
+      // let patientParams = new HttpParams().set("q", "__all__");
+      // let transitionSync = d3.transition().duration(5000);
+      //
+      // this.apiSvc.get("patient", patientParams, 0).subscribe(res => {
+      //   this.patientCount = res['total'];
+      //
+      //   let patientDiv = d3.selectAll("#patient").selectAll(".count-value");
+      //
+      //   patientDiv.transition(transitionSync)
+      //     // .duration(transitionTime)
+      //     .tween("text", function(_) {
+      //       let countMax = this['textContent'];
+      //       var i = <any>d3.interpolate(0, countMax);
+      //       return function(t) {
+      //         d3.select(this).text(Math.round(i(t)));
+      //       };
+      //     });
+      // });
+      //
+      // this.apiSvc.get("sample", patientParams, 0).subscribe(res => {
+      //   this.sampleCount = res['total'];
+      //
+      //   let sampleDiv = d3.select("#sample").selectAll(".count-value");
+      //
+      //   sampleDiv.transition(transitionSync)
+      //     // .duration(transitionTime * (925/5039))
+      //     .tween("text", function(_) {
+      //       let countMax = this['textContent'];
+      //       var i = <any>d3.interpolate(0, countMax);
+      //       return function(t) {
+      //         d3.select(this).text(Math.round(i(t)));
+      //       };
+      //     });
+      // });
+      //
+      // this.exptSvc.getExptCounts().subscribe(res => {
+      //   this.experimentCount = res;
+      //
+      //
+      //   let dataDiv = d3.selectAll("#dataset").selectAll(".count-value");
+      //
+      //   dataDiv.transition(transitionSync)
+      //     // .duration(transitionTime * (312/5039))
+      //     .tween("text", function(_) {
+      //       let countMax = this['textContent'];
+      //       var i = <any>d3.interpolate(0, countMax);
+      //       return function(t) {
+      //         d3.select(this).text(Math.round(i(t)));
+      //       };
+      //     });
+      // })
+this.getSummaryCounts();
+    }
   }
-}
+
+  getSummaryCounts() {
+    let patientParams = new HttpParams().set("q", "__all__");
+    let transitionSync = d3.transition().duration(5000);
+
+
+    forkJoin(this.apiSvc.get("patient", patientParams, 0), this.apiSvc.get("sample", patientParams, 0), this.exptSvc.getExptCounts())
+      .pipe(
+        map(([patients, samples, expts]) => {
+          // patients
+          this.patientCount = patients['total'];
+
+          let patientDiv = d3.selectAll("#patient").selectAll(".count-value");
+
+          patientDiv.transition(transitionSync)
+            // .duration(transitionTime)
+            .tween("text", function(_) {
+              let countMax = this['textContent'];
+              var i = <any>d3.interpolate(0, countMax);
+              return function(t) {
+                d3.select(this).text(Math.round(i(t)));
+              };
+            });
+
+          // Samples
+          this.sampleCount = samples['total'];
+
+          let sampleDiv = d3.select("#sample").selectAll(".count-value");
+
+          sampleDiv.transition(transitionSync)
+            // .duration(transitionTime * (925/5039))
+            .tween("text", function(_) {
+              let countMax = this['textContent'];
+              var i = <any>d3.interpolate(0, countMax);
+              return function(t) {
+                d3.select(this).text(Math.round(i(t)));
+              };
+            });
+          // experiments
+          this.experimentCount = expts;
+
+
+          let dataDiv = d3.selectAll("#dataset").selectAll(".count-value");
+
+          dataDiv.transition(transitionSync)
+            // .duration(transitionTime * (312/5039))
+            .tween("text", function(_) {
+              let countMax = this['textContent'];
+              var i = <any>d3.interpolate(0, countMax);
+              return function(t) {
+                d3.select(this).text(Math.round(i(t)));
+              };
+            });
+        })
+      );
+  }
 
 }
