508b6569fc1c2d297481e0381e5f52f1d075140c
Laura Hughes <flaneuse@users.noreply.github.com>
2019-11-07 13:12:11 -0800

    remove console.logs

--- sample-viewer/src/app/patient-page/patient-nav/patient-nav.component.ts
+++ sample-viewer/src/app/patient-page/patient-nav/patient-nav.component.ts
@@ -32,12 +32,9 @@ export class PatientNavComponent implements AfterViewInit, OnInit{
   }
 
   ngOnInit() {
-    console.log(this.links)
-    console.log(this.expts)
     if (this.expts && this.expts.length > 0) {
       this.links.push({ 'key': 'data', 'values': this.expts.map((d: any) => d.measurementCategory) }).append(this.links_end);
     }
-    console.log(this.links)
   }
 
   ngAfterViewInit() {
