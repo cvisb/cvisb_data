936bd3cfae426ae2179df60c419ffd1acf4dd421
Laura Hughes <flaneuse@users.noreply.github.com>
2019-11-07 10:46:58 -0800

    split sources L/R

--- sample-viewer/src/app/dataset-page/dataset-source/dataset-source.component.ts
+++ sample-viewer/src/app/dataset-page/dataset-source/dataset-source.component.ts
@@ -16,11 +16,20 @@ export class DatasetSourceComponent implements OnInit {
   sourcesPanelState: boolean = true;
   expansionPanelHeight: string = "42px";
   @Input() sources: Source[];
+  sources_left: Source[];
+  sources_right: Source[];
 
   constructor() { }
 
   ngOnInit() {
     this.sources = this.sources.sort((a, b) => b.percent - a.percent);
+    let sources_length = this.sources.length;
+    if(sources_length > 4) {
+      this.sources_left = this.sources.slice(0, Math.ceil(sources_length/2));
+      this.sources_left = this.sources.slice(Math.floor(sources_length/2), sources_length);
+    } else {
+      this.sources_left = this.sources;
+    }
     this.colorScale = d3.scaleSequential(d3.interpolateGreys)
       .domain([0, 1]);
   }
