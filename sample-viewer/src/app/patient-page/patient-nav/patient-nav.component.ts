a459ee4ca5b05c9fbeb8f2b3a695e119284104ee
Laura Hughes <flaneuse@users.noreply.github.com>
2019-11-07 13:04:00 -0800

    fix syntax

--- sample-viewer/src/app/patient-page/patient-nav/patient-nav.component.ts
+++ sample-viewer/src/app/patient-page/patient-nav/patient-nav.component.ts
@@ -32,9 +32,10 @@ export class PatientNavComponent implements AfterViewInit, OnInit{
   }
 
   ngOnInit() {
+    console.log(this.links)
     console.log(this.expts)
     if (this.expts && this.expts.length > 0) {
-      this.links.append({ 'key': 'data', 'values': this.expts.map((d: any) => d.measurementCategory) }).append(this.links_end);
+      this.links.push({ 'key': 'data', 'values': this.expts.map((d: any) => d.measurementCategory) }).append(this.links_end);
     }
     console.log(this.links)
   }
