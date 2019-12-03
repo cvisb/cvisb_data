47c917cd8544e2efd7238c3fd5379222baa737d8
Laura Hughes <flaneuse@users.noreply.github.com>
2019-12-03 14:05:41 -0800

    Update dataset-page.module.ts

--- sample-viewer/src/app/dataset-page/dataset-page.module.ts
+++ sample-viewer/src/app/dataset-page/dataset-page.module.ts
@@ -8,13 +8,13 @@ import { FileMetadataComponent } from '../dataset/file-metadata/file-metadata.co
 import { MdObjectComponent } from '../dataset/file-metadata/md-object/md-object.component';
 import { DatasetCitationComponent } from './dataset-citation/dataset-citation.component';
 import { CuratedAlignmentsComponent } from './curated-alignments/curated-alignments.component';
+import { DatasetSourceComponent } from './dataset-source/dataset-source.component';
 
 // --- modules ---
 import { FileListModule } from '../file-list/file-list.module';
 import { RouterModule } from '@angular/router';
 import { EmbedJsonldModule } from '../embed-jsonld/embed-jsonld.module';
 import { MaterialModule } from '../material.module';
-import { DatasetSourceComponent } from './dataset-source/dataset-source.component';
 import { FormatCitationModule } from '../format-citation/format-citation.module';
 import { FormatPublisherModule } from '../format-publisher/format-publisher.module';
 import { DatasetSummaryModule } from '../dataset-summary/dataset-summary.module';
