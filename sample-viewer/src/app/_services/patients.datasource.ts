b4b586108fbda4b722329466d0a02412b56a4ff0
Laura Hughes <flaneuse@users.noreply.github.com>
2019-12-10 14:34:01 -0800

    remove console.logs

--- sample-viewer/src/app/_services/patients.datasource.ts
+++ sample-viewer/src/app/_services/patients.datasource.ts
@@ -66,7 +66,6 @@ export class PatientsDataSource implements DataSource<Patient> {
         // tap(x => console.log(x))
       ))
     ).subscribe(patientList => {
-      console.log(patientList)
       this.resultCountSubject.next(patientList['total']);
       this.patientsSubject.next(patientList['hits']);
       this.patientSvc.patientsSummarySubject.next(patientList['summary']);
