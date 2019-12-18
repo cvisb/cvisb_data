ec2cbf505031de3f5ecadddffcc303ee51d90800
Laura Hughes <flaneuse@users.noreply.github.com>
2019-12-18 15:23:57 -0800

    updated yaml python package --> changed syntax

--- sample-viewer-api/src/static/schemas/scripts/yaml2json.py
+++ sample-viewer-api/src/static/schemas/scripts/yaml2json.py
@@ -7,7 +7,7 @@ import yaml
 def convert(yaml_file, outfile=None):
     '''Convert input yaml file as JSON file.'''
     with open(yaml_file) as in_f:
-        schema_data = yaml.load(in_f)
+        schema_data = yaml.safe_load(in_f)
         outfile = os.path.splitext(yaml_file)[0] + '.jsonld'
         with open(outfile, 'w') as out_f:
             json.dump(schema_data, out_f, indent=2)
