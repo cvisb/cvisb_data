2da4b08d358590ba4ec7943fa0e8a236f79e404e
Laura Hughes <flaneuse@users.noreply.github.com>
2020-02-17 15:24:33 -0800

    switch from ng-select to mat-autocomplete w/ mat-chips

--- sample-viewer/src/app/filters/filter-patient-id/filter-patient-id.component.ts
+++ sample-viewer/src/app/filters/filter-patient-id/filter-patient-id.component.ts
@@ -1,9 +1,15 @@
-import { Component, OnInit, Input, ViewChild, OnDestroy } from '@angular/core';
+import { Component, OnInit, Input, ViewChild, OnDestroy, ElementRef } from '@angular/core';
 
 import { Observable, Subscription } from 'rxjs';
 import { AuthState } from '../../_models';
+import { COMMA, ENTER, SPACE } from '@angular/cdk/keycodes';
+import { FormControl } from '@angular/forms';
+import { MatAutocompleteSelectedEvent, MatAutocomplete } from '@angular/material/autocomplete';
+import { MatChipInputEvent } from '@angular/material/chips';
+import { map, startWith } from 'rxjs/operators';
+import { cloneDeep } from 'lodash';
 
-import { RequestParametersService, AuthService } from '../../_services';
+import { RequestParametersService, AuthService, CheckIdsService } from '../../_services';
 
 @Component({
   selector: 'app-filter-patient-id',
@@ -12,35 +18,45 @@ import { RequestParametersService, AuthService } from '../../_services';
 })
 export class FilterPatientIdComponent implements OnInit, OnDestroy {
   @Input() endpoint: string;
-  @Input() patients: string[];
   @Input() all_patients: string[];
-  loaded_patients: string[] = [];
-  bufferSize = 50;
-  numberOfItemsFromEndBeforeFetchingMore = 10;
+  @ViewChild('idInput') idInput: ElementRef<HTMLInputElement>;
+  @ViewChild('auto') matAutocomplete: MatAutocomplete;
+
   loading = false;
-  @ViewChild('selectpatients', { static: false }) public ngSelect: any;
   authorized$: Observable<AuthState>;
 
-  selectedPatients: string[] = [];
+  // autocomplete
+  selectable = true;
+  removable = true;
+  separatorKeysCodes: number[] = [ENTER, COMMA, SPACE];
+  idCtrl = new FormControl();
+  filteredIDs: Observable<string[]>;
+  selectedIDs: string[] = [];
+  num2Display: number = 6;
+
+  // G- or S- id checkboxes
   inclContacts: boolean;
   inclSID: boolean;
   inclGID: boolean;
 
+  // subscription tracking
   patientSubscription: Subscription;
   sampleSubscription: Subscription;
 
   constructor(
     private requestSvc: RequestParametersService,
-    private authSvc: AuthService
+    private authSvc: AuthService,
+    private idSvc: CheckIdsService
   ) {
     this.authorized$ = this.authSvc.authState$;
+
+    // listen for typing changes in ID field dynamically to filter as type
+    this.filteredIDs = this.idCtrl.valueChanges.pipe(
+      startWith(null),
+      map((id: string | null) => id ? this._filter(id) : (this.all_patients ? this.all_patients.slice(0, this.num2Display) : null)));
   }
 
   ngOnInit() {
-    if (this.all_patients && this.all_patients.length > 0) {
-      this.loaded_patients = this.all_patients.slice(0, this.bufferSize);
-    }
-
     switch (this.endpoint) {
       case "patient":
         this.patientSubscription = this.requestSvc.patientParamsState$.subscribe(params => {
@@ -65,25 +81,85 @@ export class FilterPatientIdComponent implements OnInit, OnDestroy {
     }
   }
 
+  // type to add a mat-chip
+  add(event: MatChipInputEvent): void {
+    const input = event.input;
+    const value = event.value;
+
+    // Add an ID
+    if ((value || '').trim()) {
+      // If a string of IDs is copy / pasted, split by spaces or commas and add to array
+      this.selectedIDs = this.selectedIDs.concat(value.split(/\s+/).flatMap(d => d.split(",")).map(d => d.trim()).filter(d => d && d !== ""));
+    }
+
+    // Reset the input value
+    if (input) {
+      input.value = null;
+      // input.value = "";
+    }
+
+    this.idCtrl.setValue(null);
+
+    // execute the search
+    this.executeSearch(this.selectedIDs)
+  }
+
+  // remove a selected option
+  remove(id: string): void {
+    const index = this.selectedIDs.indexOf(id);
+
+    if (index >= 0) {
+      this.selectedIDs.splice(index, 1);
+    }
+    // execute the search
+    this.executeSearch(this.selectedIDs)
+  }
+
+  // Select from the list of options
+  selected(event: MatAutocompleteSelectedEvent): void {
+    this.selectedIDs.push(event.option.viewValue);
+    // execute the search
+    this.executeSearch(this.selectedIDs)
+    // this.idInput.nativeElement.value = "";
+    this.idInput.nativeElement.value = null;
+    this.idCtrl.setValue(null);
+  }
+
+  // Filter down the autocomplete options
+  private _filter(value: string): string[] {
+    const filterValue = value.toLowerCase();
+
+    return (this.all_patients.filter(id => id && id.toLowerCase().indexOf(filterValue) > -1).slice(0, this.num2Display));
+  }
 
   // Used to reset, when the filters are cleared.
   checkParams(params) {
     if (params.length === 0) {
-      this.selectedPatients = null;
+      this.selectedIDs = [];
     }
   }
 
+  // Executes the search
+  executeSearch(patientIDs: string[]) {
+    let ids = cloneDeep(patientIDs);
 
-  filterPatientIDs(patientIDs: string[]) {
+    // Add in additional aliases for the id
+    ids.forEach(d => {
+      let correctedID = this.idSvc.checkPatientID(d);
+      if (correctedID.status !== 200 && correctedID.id) {
+        ids.push(correctedID.id);
+      }
+    })
+
     if (this.inclContacts) {
       this.requestSvc.updateParams(this.endpoint,
         {
-          field: 'patientID', value: patientIDs, orSelector: {
-            field: 'relatedTo', value: patientIDs
+          field: 'patientID', value: ids, orSelector: {
+            field: 'relatedTo', value: ids
           }
         });
     } else {
-      this.requestSvc.updateParams(this.endpoint, { field: 'alternateIdentifier', value: patientIDs });
+      this.requestSvc.updateParams(this.endpoint, { field: 'alternateIdentifier', value: ids });
     }
   }
 
@@ -92,25 +168,24 @@ export class FilterPatientIdComponent implements OnInit, OnDestroy {
   //   // console.log(includeContacts)
   //   // console.log(this.patients)
   //
-  //   if (this.selectedPatients && this.selectedPatients.length > 0) {
+  //   if (this.selectedIDs && this.selectedIDs.length > 0) {
   //     if (this.inclContacts) {
   //       this.requestSvc.updateParams(this.endpoint,
   //         {
-  //           field: 'patientID', value: this.selectedPatients, orSelector: {
-  //             field: 'relatedTo', value: this.selectedPatients
+  //           field: 'patientID', value: this.selectedIDs, orSelector: {
+  //             field: 'relatedTo', value: this.selectedIDs
   //           }
   //         });
   //     } else {
-  //       this.requestSvc.updateParams(this.endpoint, { field: 'patientID', value: this.selectedPatients });
+  //       this.requestSvc.updateParams(this.endpoint, { field: 'patientID', value: this.selectedIDs });
   //     }
   //   } else {
   //     this.requestSvc.updateParams(this.endpoint, { field: 'patientID', value: null });
   //   }
   // }
 
-  filterID(includeID: boolean, ID_type) {
-    // console.log(includeSID)
-
+  // Filter ID type if it exists
+  filterIDExists(includeID: boolean, ID_type: string) {
     if (includeID) {
       this.requestSvc.updateParams(this.endpoint, {
         field: ID_type, value: '_exists_'
@@ -120,52 +195,4 @@ export class FilterPatientIdComponent implements OnInit, OnDestroy {
     }
   }
 
-
-  onSearch(input) {
-    let parsed = input.term.split("\,");
-
-    if (parsed.length > 1) {
-      parsed = parsed.filter(d => d.length > 0);
-      parsed = parsed.map((d: string) => d.trim());
-
-      // check that selected patients exists before concatting
-      if (!this.selectedPatients) {
-        this.selectedPatients = [];
-      }
-
-      // Update the selection to include the typed values.
-      this.selectedPatients = this.selectedPatients.concat(parsed);
-      // clear the input text
-      // this.ngSelect.filterValue = null;
-      // this.ngSelect.active = [];
-
-      // this.ngSelect.remove(input);
-      this.filterPatientIDs(this.selectedPatients);
-    }
-  }
-
-  onScrollToEnd() {
-    this.fetchMore();
-  }
-
-  onScroll({ end }) {
-    if (this.loading || this.all_patients.length <= this.loaded_patients.length) {
-      return;
-    }
-
-    if (end + this.numberOfItemsFromEndBeforeFetchingMore >= this.loaded_patients.length) {
-      this.fetchMore();
-    }
-  }
-
-  private fetchMore() {
-    const len = this.loaded_patients.length;
-    const more = this.all_patients.slice(len, this.bufferSize + len);
-    this.loading = true;
-    // using timeout here to simulate backend API delay
-    setTimeout(() => {
-      this.loading = false;
-      this.loaded_patients = this.loaded_patients.concat(more);
-    }, 200)
-  }
 }
