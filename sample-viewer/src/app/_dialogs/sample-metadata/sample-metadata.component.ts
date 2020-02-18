fc7ee63fd7b43e13b49e0760c717ecbd3e5d3546
Laura Hughes <flaneuse@users.noreply.github.com>
2020-02-18 09:47:05 -0800

    update for angular 9

--- sample-viewer/src/app/_dialogs/sample-metadata/sample-metadata.component.ts
+++ sample-viewer/src/app/_dialogs/sample-metadata/sample-metadata.component.ts
@@ -2,8 +2,6 @@ import { Component, Inject } from '@angular/core';
 
 import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
-import SAMPLE_SCHEMA from '../../../assets/schema/sample.json';
-
 @Component({
   selector: 'app-sample-metadata',
   templateUrl: './sample-metadata.component.html',
