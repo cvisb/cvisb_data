99841a34b0b522e39d16be43b52cbd0596f290ed
Ubuntu <cyrus.afrasiabi@gmail.com>
2020-02-19 22:41:15 +0000

    removing SSR error: refused to set unsafe header cookie

--- sample-viewer/src/app/app.server.module.ts
+++ sample-viewer/src/app/app.server.module.ts
@@ -4,6 +4,18 @@ import { ServerModule, ServerTransferStateModule } from '@angular/platform-serve
 import { AppModule } from './app.module';
 import { AppComponent } from './app.component';
 
+// activate cookie for server-side rendering
+// from https://github.com/angular/angular/issues/15730#issuecomment-572992686
+import * as xhr2 from 'xhr2';
+import { XhrFactory } from '@angular/common/http';
+
+export class ServerXhr implements XhrFactory {
+  build(): XMLHttpRequest {
+    xhr2.prototype._restrictedHeaders.cookie = false;
+    return new xhr2.XMLHttpRequest();
+  }
+}
+
 @NgModule({
   imports: [
     AppModule,
@@ -11,5 +23,6 @@ import { AppComponent } from './app.component';
     ServerTransferStateModule
   ],
   bootstrap: [ AppComponent ],
+  providers: [{ provide: XhrFactory, useClass: ServerXhr }]
 })
 export class AppServerModule {}
