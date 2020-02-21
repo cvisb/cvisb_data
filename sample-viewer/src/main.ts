69027f698495b7c48433fb6e50e30b0fc64f4988
Laura Hughes <flaneuse@users.noreply.github.com>
2020-02-20 17:45:17 -0800

    testing

--- sample-viewer/src/main.ts
+++ sample-viewer/src/main.ts
@@ -5,6 +5,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+console.log(environment)
 if (environment.production) {
   enableProdMode();
 }
