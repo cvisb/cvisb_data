3c85c82d006a31fc99063e3f534379d2cd86d2e2
Laura Hughes <flaneuse@users.noreply.github.com>
2020-06-22 11:51:22 -0700

    fix sorting syntax

--- sample-viewer/src/app/_services/get-patients.service.ts
+++ sample-viewer/src/app/_services/get-patients.service.ts
@@ -348,7 +348,7 @@ export class GetPatientsService {
       .set("q", "__all__")
       .set("patientID", `"${patientID}"`)
       .set("fields", fields.join(","))
-      .set("sort", "visitCode")
+      .set("sort", "visitCode.keyword")
       .set("pageSize", "1000");
 
     return this.myhttp.get<ESResult>(environment.api_url + "/api/experiment/query", {
