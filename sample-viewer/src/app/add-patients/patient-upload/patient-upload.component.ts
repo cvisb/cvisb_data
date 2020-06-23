e6bfacec3760d6da5467fb9a6c50f62e01b25eeb
Laura Hughes <flaneuse@users.noreply.github.com>
2020-06-23 10:36:42 -0700

    sort new IDs to be added

--- sample-viewer/src/app/add-patients/patient-upload/patient-upload.component.ts
+++ sample-viewer/src/app/add-patients/patient-upload/patient-upload.component.ts
@@ -121,9 +121,12 @@ export class PatientUploadComponent implements OnInit {
 
         this.apiSvc.prepUpload("patient", "patientID", this.data2upload).subscribe(dupes => {
           console.log(dupes)
+          dupes.sort((a,b) => a > b ? -1 : 1);
           this.dupes = dupes;
           this.replacementIDs = this.data2upload.filter(d => d["_id"]).map(d => d["patientID"]);
+          this.replacementIDs.sort((a,b) => a > b ? -1 : 1);
           this.newIDs = this.data2upload.filter(d => !d["_id"]).map(d => d["patientID"]);
+          this.newIDs.sort((a,b) => a > b ? -1 : 1);
 
         })
         // Clear input so can re-upload the same file.
