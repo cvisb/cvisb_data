2f68f018292f83c065c10b8d81f23bfe0733f498
Laura Hughes <flaneuse@users.noreply.github.com>
2020-10-06 13:35:52 -0500

    tester: formgroups for filter selection

--- sample-viewer/src/app/download/download.component.ts
+++ sample-viewer/src/app/download/download.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 
 import { ActivatedRoute, Router } from '@angular/router';
-import { FormGroup, FormBuilder } from '@angular/forms';
+import { FormGroup, FormBuilder, FormArray } from '@angular/forms';
 import { GetExperimentsService } from "../_services/get-experiments.service";
 import { DownloadDataService } from "../_services/download-data.service";
 import { Subscription } from 'rxjs';
@@ -25,18 +25,24 @@ export class DownloadComponent implements OnInit, OnDestroy {
     patient: true
   });
 
-  filterForm: FormGroup = this.fb.group({
-    cohort: null
-  });
+  filterForm: FormGroup;
 
   constructor(
     private exptSvc: GetExperimentsService,
     private dwnldSvc: DownloadDataService,
     private fb: FormBuilder,
     private router: Router,
-    private route: ActivatedRoute) { }
+    private route: ActivatedRoute) {
+    this.filterForm = this.fb.group({
+      cohort: this.fb.array([]),
+      outcome: this.fb.array([])
+    })
+  }
 
   ngOnInit() {
+    let cohorts = this.filterForm.get("cohort") as FormArray;
+    let outcomes = this.filterForm.get("outcome") as FormArray;
+
     const params = this.route.snapshot.queryParams;
     console.log(params)
     this.id = this.route.snapshot.paramMap.get("id");
@@ -48,19 +54,28 @@ export class DownloadComponent implements OnInit, OnDestroy {
       this.total = results["total"];
       this.summary = results["filteredSummary"];
       this.filters = results["filters"];
+
+      results["filters"]["cohorts"].forEach(d => {
+        cohorts.push(this.fb.group(d))
+      })
+
+      results["filters"]["outcomes"].forEach(d => {
+        outcomes.push(this.fb.group(d))
+      })
     });
   }
 
-  ngOnDestroy () {
+  ngOnDestroy() {
     this.dataSubscription.unsubscribe();
   }
 
   changeRoute() {
     console.log("changingroute")
-    this.router.navigate(["/download", this.id, {location: "test"}])
+    this.router.navigate(["/download", this.id, { location: "test" }])
   }
 
   downloadData() {
+    console.log(this.filterForm.value)
     this.dwnldSvc.downloadExperiments(this.id, this.whichDataForm.get("experiment").value, this.whichDataForm.get("patient").value)
   }
 
